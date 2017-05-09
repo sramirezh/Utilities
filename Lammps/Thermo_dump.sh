cbf39ab5750b839ab9ccdee89acc5657c5362ac7
Simon_nimbus <sr802@cam.ac.uk>
2017-05-09 18:44:05 +0100

    Thermo_dump acts on a file given in the command line

--- Lammps/Thermo_dump.sh
+++ Lammps/Thermo_dump.sh
@@ -1,8 +1,10 @@
 #This script takes the log.lammps and generates a file just containing the parameters dumped by the thermo_style
 #Creates a file called parameters.dat
+#Just to check
+File=$1
 rm parameters.dat
-a=$(grep -n "Per MPI" log.lammps| awk -F":" '{print $1}') #the awk gets the first column when they are separated by :
-tail -n +$((a+1)) log.lammps > out
+a=$(grep -n "Per MPI" $File| awk -F":" '{print $1}') #the awk gets the first column when they are separated by :
+tail -n +$((a+1)) $File > out
 b=$(grep -n "Loop time" out| awk -F":" '{print $1}')
 head -$((b-1)) out> parameters.dat
 rm out
