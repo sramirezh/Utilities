d50e9535a32ba0ce34c125c82fb23a7dfce24509
Simon_nimbus <sr802@cam.ac.uk>
2017-05-26 11:14:07 +0100

    Trajectory analyzers

---
+++ Lammps/Trajectory_Analysis/Trajectory_Splitter.sh
@@ -0,0 +1,17 @@
+# This script reads the trajectory data and creates individual files for every timestep
+# The name of the file to be splitted is given as input in the command line. 
+
+rm *.cxyz
+rm Times.dat
+name=$1 
+
+
+csplit --digits=4 -z --quiet --prefix=outfile $name "/^[0-9]*$/" "{*}" #Splits based on lines that contain at the beggining a number and after that number, the line finishes
+
+#Now change all the filenames and create the file Times.dat
+for f in outfile*
+do
+var=`head -2 $f|tail -1|awk {'print $3'}` #gets the time
+echo $var>>Times.dat
+mv $f $var.cxyz
+done
