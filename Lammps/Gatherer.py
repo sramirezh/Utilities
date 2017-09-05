f829e6f69fd799e6b8b1fe4701e6b94ca632990a
Simon_nimbus <sr802@cam.ac.uk>
2017-09-05 16:57:17 +0100

    Added a correction, to take into account only N runs for the averaging

--- Lammps/Gatherer.py
+++ Lammps/Gatherer.py
@@ -2,20 +2,23 @@
 # -*- coding: utf-8 -*-
 """
 script to be used after running Gatherer.sh
-
+Notice the definition of N, the number of runs to be 
+taken into account to compute the averages
 
 @author: sr802
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
+N=5 #Number of runs to average
+
 Times=np.loadtxt("../times.dat",dtype=int)
 x=np.size(Times)
 
 #Opening the first array
-data=np.loadtxt("full-mean."+str(int(Times[0])))
-
-for k in xrange(1,x): #Runs over the sampled times.
+data=np.loadtxt("full-mean."+str(int(Times[x-N])))
+for k in xrange(x-N+1,x): #Runs over the sampled times.
+               
     print("Reading configuration %d of %d" %(k,x-1))
     #Reading the Results 
     name="full-mean."+str(int(Times[k]))
@@ -23,11 +26,10 @@ for k in xrange(1,x): #Runs over the sampled times.
     data[:,2::]=data[:,2::]+array[:,2::]
     
 #Normalizing
-data[:,2::]=data[:,2::]/x
+data[:,2::]=data[:,2::]/N
 np.savetxt("Averages.dat",data)
-
-#plt.plot(data[:,1],data[:,3])
-#plt.plot(array[:,1],array[:,3])
-
+plt.plot(data[:,1],data[:,3])
+plt.ylim(-0.005,0.005)
+plt.xlim(5.0,13.0)
 
     
