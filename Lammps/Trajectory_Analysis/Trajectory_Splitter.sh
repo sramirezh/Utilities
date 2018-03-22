8deb980ee3aefc6396a236e390e16b17cc6a2cfc
MAC_simon <sramirez.hinestrosa@gmail.com>
2018-03-22 18:04:27 +0100

    Th

--- Lammps/Trajectory_Analysis/Trajectory_Splitter.sh
+++ Lammps/Trajectory_Analysis/Trajectory_Splitter.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 # This script reads the trajectory data and creates individual files for every timestep
 # The name of the file to be splitted is given as input in the command line.
 #Notice that the number of particles inside the file for every step can vary.
@@ -5,10 +6,8 @@
 ###############################################################################
 #Function definition
 ###############################################################################
-TotalSplit()
-{
+TotalSplit(){
 csplit --digits=4 -z --quiet --prefix=outfile $name "/^[0-9]*$/" "{*}" #Splits based on lines that contain at the beggining a number and after that number, the line finishes
-
 #Now change all the filenames and create the file Times.dat
 for f in outfile*
 do
@@ -18,8 +17,8 @@ mv $f $var.cxyz
 done
 }
 
-PartialSplit()
-{
+PartialSplit(){
+  
   NAtoms=`head -1 $name` #Number of atoms
   Nlines=$(($NAtoms+2))
   head -$Nlines trajectory.xyz > 0.cxyz
