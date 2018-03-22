8d1ff1ac4d51c82340b4361811dc8d50d2b4600f
MAC_simon <sramirez.hinestrosa@gmail.com>
2018-03-22 17:46:35 +0100

    Had a typo

--- Lammps/Analyser.sh
+++ Lammps/Analyser.sh
@@ -16,7 +16,7 @@ printf "\n######################################################################
 echo "Analyzing the trajectory File"
 echo "##########################################################################"
 bash $dir/Trajectory_Analysis/Trajectory_Splitter.sh -i trajectory.xyz -s Initial
-python $dir/Trajectory_Analysis/Trajectory_Analiser.py
+python $dir/Trajectory_Analysis/Trajectory_Analyser.py
 printf "\nGenerated 0.xyz and Zshift.dat \n"
 
 printf "\n##########################################################################\n"
