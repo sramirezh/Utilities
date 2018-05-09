96766deff90290cb346e92c84edff14a5af2050f
Simon_nimbus <sr802@cam.ac.uk>
2018-05-09 18:24:58 +0100

    Improving all the computations

--- Lammps/PDP/sheet_analysis.py
+++ Lammps/PDP/sheet_analysis.py
@@ -10,11 +10,127 @@ Returns:
 @author: simon
 """
 
-
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+
+
+"""
+*******************************************************************************
+Functions
+*******************************************************************************
+"""
+
+def build_data():
+    """
+    Function to initialise all the elements of the class
+    """
+
+    interactions=[]
+    with open("Statistic_summary.dat", 'r') as f:
+      lines = f.readlines()
+    
+    i=0
+    nproperties=11 #The number of properties per force in the input file (TO IMPROVE)
+    count=0
+    
+    while i<len(lines):
+        if re.search("\AE_*",lines[i] ): #Finding the LJ parameters
+            interactions.append(LJInteraction(re.findall(r"[-+]?\d*\.?\d+", lines[i])))
+            print "Analysing %s"%lines[i]
+            i=i+1
+            count+=1
+        else: 
+            if re.search("\AdDP*",lines[i] ):
+                interactions[-1].addforce(lines[i])
+                i+=1
+                properties=[]
+                for j in xrange(nproperties):
+                    properties.append(lines[i])
+                    i+=1
+                interactions[-1].addproperties(properties)
+                    
+            i+=1
+        
+    return interactions
+
+
+def parameter_finder(List, String):
+    """
+    Finds a string on a List and returns the position on the list
+    """
+    cont=0
+    indexes=[]
+    for s in List:
+        if String in s: 
+            indexes.append(cont) 
+        cont+=1
+    length=len(indexes)
+    if length>1: print "There were several ocurrences"
+    if length==0: print "No ocurrences found"
+    return indexes 
+
+
+
+"""
+*******************************************************************************
+CLASS DEFINITION
+*******************************************************************************
+"""
+class LJInteraction(object):
+    """
+    Every pair of LJ interactions has its own
+    """
+    
+    def __init__(self,lj_parameters):
+        self.epsilon=float(lj_parameters[0])
+        self.sigma=float(lj_parameters[1])
+        self.forces=[]
+        self.properties=[]
+        self.property_names=[]
+
+    def addforce(self,new_force):
+        force=float(new_force.strip('\n/dDP'))/1000
+        self.forces.append(force)
+        
+    def addproperties(self,properties):
+        values=[]
+        names=[]
+        for element in properties:
+            name,value=element.strip("\n").split("=")
+            name=name.replace(" ","_")
+            values.append(float(value))
+            names.append(name)
+        self.properties.append(values)
+        self.property_names.append(names)
+            
+        
+    def compute_mobility(self):
+        self.mobility=[]
+        self.mob_rg=[] #Mobility over Rg
+        count=0
+        index_vx=parameter_finder(self.property_names[count],"vx_relative")[0]
+        index_rg=parameter_finder(self.property_names[count],"rg_ave")[0]
+        for force in self.forces:
+            if force!=0:
+                velocity=self.properties[count][index_vx]
+                rg=self.properties[count][index_rg]
+                mobility=velocity/force
+                self.mobility.append(mobility)
+                self.mob_rg.append(mobility/rg)
+            count+=1
+            
+    def get_property(self,name):
+        """
+        function to get the specific property
+        """
+        
+
+
+
+
 #import argparse
 #
 #
@@ -50,84 +166,72 @@ data1=data.as_matrix()
 
 datasol=pd.read_csv("Statistic_summary.dat",sep="\t",header=0)
 
-"""
-*******************************************************************************
-CLASS DEFINITION
-*******************************************************************************
-"""
-class LJInteraction(object):
-    """
-    Every pair of LJ interactions has its own
-    """
-    
-    def __init__(self,lj_parameters):
-        self.epsilon=float(lj_parameters[0])
-        self.sigma=float(lj_parameters[1])
-        self.forces=[]
-        self.properties=[]
-        self.propertie_names=[]
-        
-    def addforce(self,new_force):
-        force=float(new_force.strip('\n/dDP'))/1000
-        self.forces.append(force)
-        
-    def addproperties(self,properties):
-        values=[]
-        names=[]
-        for element in properties:
-            name,value=element.strip("\n").split("=")
-            values.append(float(value))
-            names.append(name)
-        self.properties.append(values)
-        self.propertie_names.append(names)
+       
     
 """
 *******************************************************************************
-Functions
+Main program
+*******************************************************************************
+"""
+     
+interactions=build_data()
+
+
+directory="plots/individual"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+
+"""
+*******************************************************************************
+Building the averaged data
 *******************************************************************************
 """
 
-def buil_data():
-    """
-    Function to initialise all the elements of the class
-    """
 
-    interactions=[]
-    with open("Statistic_summary.dat", 'r') as f:
-      lines = f.readlines()
+
+data=[]
+i_concentration=parameter_finder(interactions[0].property_names[0],"concentration")[0]
+for interaction in interactions:    
+    name='E_%sS_%s '%(interaction.epsilon,interaction.sigma)
+    interaction.compute_mobility()
     
-    i=0
-    nproperties=11 #The number of properties per force in the input file (TO IMPROVE)
-    inter=[]
-    count=0
+    ave_mobility=np.average(interaction.mobility)
+    ave_concentration_rg=np.average()
+    data_interaction=[name,ave_mobility, ave_concentration_rg ]
     
-    while i<len(lines):
-        if re.search("\AE_*",lines[i] ): #Finding the LJ parameters
-            interactions.append(LJInteraction(re.findall(r"[-+]?\d*\.?\d+", lines[i])))
-            force=[]
-            print lines[i]
-            i=i+1
-            count+=1
-        else: 
-            if re.search("\AdDP*",lines[i] ):
-                interactions[-1].addforce(lines[i])
-                i+=1
-                properties=[]
-                for j in xrange(nproperties):
-                    properties.append(lines[i])
-                    i+=1
-                interactions[-1].addproperties(properties)
-                    
-            i+=1
-        
-    
+
+
+
+
 """
 *******************************************************************************
-CLASS DEFINITION
+Individual runs plots
 *******************************************************************************
 """
-        
 
+def plot_force_individuals(interactions):
+    """
+    Plots the parameters from statistic_summary for each force, for each interaction
+    """
+    n_properties=len(interactions[0].properties[0]) #Number of properties
+    
+    for property_index in xrange(n_properties):
+        plt.figure()
+        for ljpair in interactions:
+            n=0
+            re2=[]
+            force_list=[]
+            for force in ljpair.forces:
+                re2.append(ljpair.properties[n][property_index]) #end to end radious squared
+                force_list.append(force)
+                n+=1
+            plt.plot(force_list,re2,label='$\epsilon$=%s $\sigma$=%s '%(ljpair.epsilon,ljpair.sigma))
+            #plt.legend("" %(ljpair.epsilon,ljpair.sigma))
+        name=interactions[-1].property_names[0][property_index].replace(" ","_")
+        plt.title(name)
+        plt.legend()
+        plt.savefig("plots/individual/%s.pdf"%name)
 
 
 #
