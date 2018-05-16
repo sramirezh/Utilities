666e57183bf8f0d6389bd5aadac7cad6326fc32f
Simon_nimbus <sr802@cam.ac.uk>
2018-05-16 10:33:46 +0100

    Better way to organize

--- Lammps/PDP/Bash_Scripts/c_force.sh
+++ Lammps/PDP/Bash_Scripts/c_force.sh
@@ -1,21 +1,22 @@
 #Due to the depths analysed, this has to be run in the main project folder, i.e where
 #The files N_1  N_30  N_90 are.
-_LISPDIRS=`find . -maxdepth 3 -mindepth 3 -type d | sort`
+#_LISPDIRS=`find . -maxdepth 3 -mindepth 3 -type d | sort`
+Files_tip=`find .  -name "tip_behaviour.pdf"  -path "*/E_*"`
+Files_radial=`find .  -name "radial_distribution.pdf"  -path "*/E_*"`
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )" #To get the absolute path of this script
 CurrentPath=$(pwd)
 dirname=plot_force
-mkdir $dirname
+mkdir  $dirname 2>dev/null
 echo This path is set as the starting directory
 echo $CurrentPath
-for dir in ${_LISPDIRS}; do
-    cd ${dir}
- 	Number=$(echo $dir | awk -F  "/" '{print $2}')
-	Interaction=$(echo $dir | awk -F  "/" '{print $3}')
-	Force=$(echo $dir | awk -F  "/" '{print $4}')
+for file in ${Files_tip}; do
+	echo $file
+ 	Number=$(echo $file | awk -F  "/" '{print $2}')
+	Interaction=$(echo $file | awk -F  "/" '{print $3}')
+	Force=$(echo $file | awk -F  "/" '{print $4}')
 	tag=$(echo "${Number}_${Interaction}")
-	mkdir -p $CurrentPath/$dirname/$Force/tip
-	mkdir -p $CurrentPath/$dirname/$Force/rd
-	cp tip_behaviour.pdf $CurrentPath/$dirname/$Force/tip/${tag}.pdf 
-	cp radial_distribution.pdf  $CurrentPath/$dirname/$Force/rd/${tag}.pdf
-    cd $CurrentPath
+	echo $Number
+	echo $Force
+#	mkdir -p $CurrentPath/$dirname/$Force/tip 2>dev/null
+	#cp $file $CurrentPath/$dirname/$Force/tip/${tag}.pdf 
 done
