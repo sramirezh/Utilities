44bd64ac031ff755679c0516ef1b33a129f03f19
sramirezh <sr802@cam.ac.uk>
2018-05-16 13:41:35 +0100

    computes statistics for several interactions

--- Lammps/PDP/Statistics/force_compute_statistics.sh
+++ Lammps/PDP/Statistics/force_compute_statistics.sh
@@ -1,9 +1,19 @@
 #Runs dp_poly and FastAvearger.py for every force
 #It has to be run inside E*
+Interactions=$@
+CurrentPath=$(pwd)
+for interaction in $Interactions;
+	do
+	
+	echo $interaction
+	cd $interaction
 	for force in dD*/;
 		do
+		echo $force
 		cd $force
 		/nodescratch/frenkelscratch/sr802/DiffusioP/programs/dp_poly -s 100000 -n 10000000
 		python ~/Utilities/Others/Statistics/FastAverager.py vdata.dat
 		cd ..
 	done
+	cd $CurrentPath
+done
