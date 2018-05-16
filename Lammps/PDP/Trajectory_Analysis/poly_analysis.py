9df2077d9857ad00ef1108bcd27533c3b4f7d77c
Simon_nimbus <sr802@cam.ac.uk>
2018-05-16 17:14:42 +0100

    fixed a bug loading linux module

--- Lammps/PDP/Trajectory_Analysis/poly_analysis.py
+++ Lammps/PDP/Trajectory_Analysis/poly_analysis.py
@@ -31,7 +31,7 @@ import os
 import sys
 
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../')) #This falls into Utilities path
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This falls into Utilities path
 from Lammps.linux import bash_command
 
 
