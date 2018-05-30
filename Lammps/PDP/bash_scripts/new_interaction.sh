188763aafd8585d16af9d8d7c1feeaeecb30eb80
sramirezh <sr802@cam.ac.uk>
2018-05-30 16:34:16 +0100

    Renaming the qsub run

--- Lammps/PDP/bash_scripts/new_interaction.sh
+++ Lammps/PDP/bash_scripts/new_interaction.sh
@@ -8,6 +8,7 @@ Sigma=$2
 
 name=$(echo "E_${Epsilon}_S_${Sigma}")
 echo $name
+rm -r $name
 cp -r Template $name
 
 cd $name
@@ -16,6 +17,20 @@ cd $name
 sed 's/$Epsilon/'$Epsilon'/g' in.interaction>output
 sed 's/$Sigma/'$Sigma'/g' output>in.interaction
 rm output
-#Equilibration qsub
-sed 's/$Flag/'$name'/g' dSetBox/dSetBox.qsub1 >dSetBox/dSetBox.qsub
-rm dSetBox/dSetBox.qsub1
+
+#Modifying all the qsub
+files=`find . -name "*.qsub"`
+for file in $files; 
+	do 
+	echo 'Modifying' $file
+	absolute_path="$( cd "$(dirname "$file")" ; pwd -P )"
+	
+	Number=$(echo $absolute_path | awk -F  "/" '{print $(NF-3)}')
+	Interaction=$(echo $absolute_path | awk -F  "/" '{print $(NF-1)}')
+	Sim_type=$(echo $absolute_path | awk -F  "/" '{print $(NF)}')
+	name=$(echo "#PBS -N ${Number}_${Epsilon}_${Sigma}_${Sim_type}")
+	sed -i "s/#PBS -N.*/${name}/g" $file
+done
+
+
+
