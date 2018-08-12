942f1943c2a4aaadae31d5f96a169b2055cf5d95
MAC_simon <sramirez.hinestrosa@gmail.com>
2018-08-12 15:09:26 +0100

    R_inside only for dDP simulations

--- Lammps/PDP/bash_scripts/R_inside_interaction.sh
+++ Lammps/PDP/bash_scripts/R_inside_interaction.sh
@@ -1,7 +1,7 @@
 #Run inside every iaxx
-#Copies the mt.data from every directory and 
+#Copies the mt.data from every directory and
 
-for folder in */ ; do
+for folder in dDP*/ ; do
 	cd $folder
 	echo $folder
 	cp ../initial_conf/mt.data .
