1c3bb4b44ebea123a9eb457ec6e0a5b12700b3cc
Simon_nimbus <sr802@cam.ac.uk>
2018-09-03 12:18:45 +0100

    added comments

--- Lammps/PDP/statistics/compute_statistics.sh
+++ Lammps/PDP/statistics/compute_statistics.sh
@@ -1,8 +1,8 @@
 #This script gets all the statistical properties by running dp_poly and FastAvearger.py for every force
 #It has to be run inside every N_X
-s=$1
-d=$2
-n=$3
+s=$1 #Initial step
+d=$2 #Interval
+n=$3 #FinalTimestep
 
 CurrentPath=$(pwd)
 rm Statistic_summary.dat 2>/dev/null
@@ -18,7 +18,7 @@ for f in E_*/;
 		echo " " >>$CurrentPath/Statistic_summary.dat
 		echo $force >>$CurrentPath/Statistic_summary.dat
 		cd $force
-		/nodescratch/frenkelscratch/sr802/DiffusioP/programs/dp_poly -s $s -d $d -n $n 
+		/nodescratch/frenkelscratch/sr802/DiffusioP/programs/dp_poly -s $s -d $d -n $n
 		cat average_info.dat >>$CurrentPath/Statistic_summary.dat
 		python ~/Utilities/Others/Statistics/FastAverager.py vdata.dat
 		cat statistics.dat >>$CurrentPath/Statistic_summary.dat
