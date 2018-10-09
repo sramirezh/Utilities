8ae284f2cbfe961363ce2b4752e5a750811cc6a7
Simon_nimbus <sr802@cam.ac.uk>
2018-10-09 19:08:16 +0100

    compute_statistics is now functional

--- Lammps/PDP/statistics/compute_statistics.py
+++ Lammps/PDP/statistics/compute_statistics.py
@@ -14,7 +14,8 @@ import numpy as np
 import sys
 import time
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This falls into Utilities path
+Utilities_path=os.path.join(os.path.dirname(__file__), '../../../')
+sys.path.append(Utilities_path) #This falls into Utilities path
 import Lammps.core_functions as cf
 
 s=0#Initial step
@@ -29,12 +30,24 @@ dir_path = os.path.dirname(os.path.realpath(__file__))#Path of this python scrip
 #onlyfiles = [f for f in os.listdir(cwd) if os.isfile(os.join(cwd, f))]
 
 
+def make_dp_poly():
+    
+    path_dp_poly=Utilities_path+"Lammps/PDP/f_programs/"
+    out, error = cf.bash_command("""make -C %s -f Makefile""" %path_dp_poly)
+    print out, error
+    path_dp_poly+="dp_poly"
+    return path_dp_poly
+
 
 
 numbers=[]
 os.chdir(cwd)
 directories=glob.glob('E_*')
 f=open(cwd+"/Statistics_summary.dat",'w')
+
+path_dp_poly=make_dp_poly()
+path_to_averager=Utilities_path+"Others/Statistics/FastAverager.py"
+
 for directory in directories:
     
     f.write( "############################################################################\n")
@@ -50,10 +63,14 @@ for directory in directories:
         os.chdir(force)
         files=glob.glob('conf/*.gz')
         #n=int(cf.extract_digits(files)[-1])
-        out,error=cf.bash_command("../../programs/dp_poly -s %s -d %s -n %s" %(s,d,n))
+        
+        #Results from dp_poly
+        out,error=cf.bash_command("%s -s %s -d %s -n %s" %(path_dp_poly,s,d,n))
         with open("average_info.dat",'r') as ave_info:
             f.writelines(ave_info.readlines())
-        cf.bash_command("""python ~/Utilities/Others/Statistics/FastAverager.py vdata.dat --min %s""" %min)
+            
+        #Results from Fast averager    
+        out, error = cf.bash_command("""python %s vdata.dat --min %s""" %(path_to_averager,dmin))
         with open("statistics.dat",'r') as ave_info:
             f.writelines(ave_info.readlines()[1:])
         
