81ac7a77cebe8faa8ad507acd7a98ddf4da6cc4d
Simon_nimbus <sr802@cam.ac.uk>
2019-02-22 17:32:24 +0000

    Computes the histogram

--- Lammps/PDP/trajectory_analysis/first_n_analysis.py
+++ Lammps/PDP/trajectory_analysis/first_n_analysis.py
@@ -18,6 +18,8 @@ from scipy.spatial.distance import pdist,squareform
 import re
 import glob 
 import warnings
+import itertools
+
 warnings.filterwarnings("ignore")
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This falls into Utilities path
@@ -60,14 +62,6 @@ def read_box_limits(log_name):
 
     return volume,limits
 
-def xyz_to_lammps(file_xyz,log_name):
-    """
-    This could be made of a function that reads the coordinates and then writes as lammps
-    """
-    
-    return
-
-   
 
 
 def xyz_splitter(file_name):
@@ -77,6 +71,138 @@ def xyz_splitter(file_name):
     return
 
 
+
+def com_pbc(positions,box_length):
+    """
+    Computes the center of mass for xyz coordinates in a box with periodic boundary conditions,
+    using the procedure in https://en.wikipedia.org/wiki/Center_of_mass#Systems_with_periodic_boundary_conditions
+    
+    Args:
+        positions a matrix with the [X Y Z] coordinates
+        box_length a vector with Lx Ly Lz
+    """
+    
+    com=np.zeros(3)
+    for i in xrange(3):
+        
+        theta=positions[:,i]/box_length[i]*2*np.pi
+        epsilon=np.cos(theta)
+        sigma=np.sin(theta)
+        theta_new=np.arctan2(-np.average(sigma),-np.average(epsilon))+np.pi
+        com[i]=box_length[i]*theta_new/(2*np.pi)
+    
+    return com
+
+
+
+def shift_coordinates(positions,box_length,shift):
+    """
+    Shifting coordinates in PBC
+    
+    args:
+        positions a matrix with the [X Y Z] coordinates
+        box_length a vector with Lx Ly Lz
+        shift a vector with the shifs in each direction
+    Returns:
+        new_pos shifted coordinates
+    """
+    n_particles,m=np.shape(positions)
+    new_pos=np.zeros((n_particles,3))
+    
+    for dim in xrange(3):
+        for i in xrange(n_particles):
+            if shift[dim]<0:
+                new_pos[i,dim]=(positions[i,dim]-shift[dim])-np.rint(0.5*(positions[i,dim]-shift[dim])/box_length[dim])*box_length[dim]
+            else:
+                new_pos[i,dim]=(positions[i,dim]-shift[dim])-np.rint(0.5*(positions[i,dim]-shift[dim]-box_length[dim])/box_length[dim])*box_length[dim]
+                
+    return new_pos
+
+
+def write_trajectory(file_name,frame,data):
+    """
+    Creates a xyz trajectory file with variable number of particles per time step
+    Args:
+        file_name name of the trajectory file
+        frame number of the frame or timestep.
+        data matrix containing [type X Y Z]
+    """
+    n_atoms,m=np.shape(data)
+    f=open(file_name,'ab') 
+    f.write("%s \n" %n_atoms)
+    f.write("frame:%s \n" %frame)
+    np.savetxt(f,data,fmt='%d %f %f %f')
+    f.close()
+    
+
+def nearest_per_type(data):
+    """
+    Evaluates what is the nearest particle to each monomer per type,
+    types as in the trajectory file from LAMMMPS [1-solvent, 2-solute, 3-polymer]
+    
+    Args:
+        data: Contains the [Type X Y Z]
+        
+    Returns:
+        nearest_solvent a list with the distances of the nearest solvents to monomers 
+        nearest_solute a list with the distances of the nearest solutes to monomers
+    """
+
+    n,m=data.shape
+    #Particle indexes
+    p_types=np.array([1,2,3])
+    p_types_frame=np.unique(data[:,0]).astype(int)
+    particles=[np.where(data[:,0]==j)[0] for j in p_types]
+    pos=data[:,1::]
+
+    dist=squareform(pdist(pos))
+    np.fill_diagonal(dist, 1000) #To avoid self contributions
+    
+    nearest_solute=[]
+    nearest_solvent=[]
+    if 1 in p_types_frame:
+        nearest_solvent=computation(particles,p_types,dist,3,1)
+    if 2 in p_types_frame:
+        nearest_solute=computation(particles,p_types,dist,3,2)
+
+    return nearest_solvent,nearest_solute
+
+    
+
+def computation(particles,p_types,dist,i,j):
+    """
+    
+    computes the distance of the closest particle of type j around type i
+    
+    Returns:
+        An array with all the positions of the nearest particles
+    
+    """
+    i=np.where(p_types == i)[0][0]
+    j=np.where(p_types == j)[0][0]
+    
+    i_axis0=[]
+    i_axis1=[]
+    
+    for k in xrange(len(p_types)):
+        if k!=i:
+            i_axis0.append(particles[k])
+        if k!=j:
+            i_axis1.append(particles[k])
+            
+    dist = np.delete(dist,np.hstack(i_axis0), axis=0)
+    dist = np.delete(dist,np.hstack(i_axis1), axis=1)
+    
+    
+    ind_min=np.argmin(dist,axis=1)
+    
+    distances=[]
+    for l,m in enumerate(ind_min):
+        index=np.argmin(dist[:,m])
+        if l==index:
+            distances.append(dist[index,m])
+    
+    return distances
 
 
 
@@ -95,39 +221,109 @@ else:
     print "The Trajectory file was not splitted"
 
 
+trajectory_name='new_trajectory.xyz'
+
+open(trajectory_name, 'w').close()
+
+
 files = glob.glob("*.cxyz")
 files.sort(key=lambda f: int(filter(str.isdigit, f)))
-file_name=files[3]
-data=pd.read_csv(file_name,sep=" ",dtype=np.float64,skiprows=2,header=None).values
-
-volume,limits=read_box_limits(args.log)
-box_length=limits[1,:]-limits[0,:]
-
-poly_coords=data[np.where(data[:,0]==3)[0],:]
-
-#Getting the image 
-
-#Cm in x with respect to the first monomer
-n_monomers,m=np.shape(poly_coords)
-dist=np.zeros(n_monomers)
-
-n_bins=5
-partition=np.linspace(limits[0,0],limits[1,0],n_bins+1)
-xpos=poly_coords[:,1]
-occupancy=np.zeros(n_bins)
-
-for i in xrange(n_bins):
-    indexes=np.where(xpos<partition[i+1])[0]
-    occupancy[i]=len(indexes)
-    xpos=np.delete(xpos,indexes)
-
-if occupancy[0]!=0:
-    ind=np.where(occupancy==0)[0]
-    center_of_void=0
 
 
-theta=pos_x/
+nearest_solvent=[]
+nearest_solute=[]
+
+for file_name in files:    
+    data=pd.read_csv(file_name,sep=" ",dtype=np.float64,skiprows=2,header=None).values
+
+    volume,limits=read_box_limits(args.log)
+    box_length=limits[1,:]-limits[0,:]
+
+    poly_coords=data[np.where(data[:,0]==3)[0],:][:,1::]
+    all_coords=data[:,1::]
     
+    #Getting the image 
+
+    #Cm in x with respect to the first monomer
+    n_monomers,m=np.shape(poly_coords)
+    dist=np.zeros(n_monomers)
+    
+    #Main algorithm
+    com_vect=com_pbc(poly_coords,box_length)
+    
+    shift=com_vect-box_length*0.5
+    
+    new_pos=shift_coordinates(all_coords,box_length,shift)
+    data[:,1::]=new_pos
+    write_trajectory(trajectory_name,cf.extract_digits(file_name)[0],data)
+    
+    #Building the Histogram
+    
+    solvent,solute=nearest_per_type(data)
+    
+    nearest_solvent.append(solvent)
+    nearest_solute.append(solute)
+    
+
+nearest_solvent=list(itertools.chain(*nearest_solvent))    
+nearest_solute=list(itertools.chain(*nearest_solute))  
+    
+
+
+# Creating the histogram
+
+plt.hist(nearest_solvent,bins='auto')
+plt.hist(nearest_solute,bins='auto')
+
+
+#
+#
+#file_name="14.cxyz"
+#data=pd.read_csv(file_name,sep=" ",dtype=np.float64,skiprows=2,header=None).values
+#
+#
+#volume,limits=read_box_limits(args.log)
+#box_length=limits[1,:]-limits[0,:]
+#
+#poly_coords=data[np.where(data[:,0]==3)[0],:][:,1::]
+#all_coords=data[:,1::]
+##Getting the image 
+#
+##Cm in x with respect to the first monomer
+#n_monomers,m=np.shape(poly_coords)
+#dist=np.zeros(n_monomers)
+#
+##Main algorithm
+#com_vect=com_pbc(poly_coords,box_length)
+#
+#shift=com_vect-box_length*0.5
+#
+#new_pos=shift_coordinates(poly_coords,box_length,shift)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#plt.close('all')
+#plt.plot(poly_coords[:,0],poly_coords[:,1],'o')
+#plt.plot(new_pos[:,0],new_pos[:,1],'or')
+##plt.xlim(limits[:,0])
+##plt.ylim(limits[:,1])
+plt.show()
+
     
     
 
