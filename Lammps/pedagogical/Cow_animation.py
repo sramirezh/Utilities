3c16aa3598ece14fdf21ee28136737a196f891bb
Simon_nimbus <sr802@cam.ac.uk>
2019-04-10 15:22:20 +0100

    random rotation

--- Lammps/pedagogical/Cow_animation.py
+++ Lammps/pedagogical/Cow_animation.py
@@ -4,6 +4,10 @@
 Created on Fri Apr  5 19:17:37 2019
 https://stackoverflow.com/questions/22566284/matplotlib-how-to-plot-images-instead-of-points
 https://stackoverflow.com/questions/31401812/matplotlib-rotate-image-file-by-x-degrees
+
+The only thing I need it the cow file and the trajectory files.
+1. with the cow I generate the rotated views
+2. with the trajectories I generate the frames
 @author: sr802
 """
 import matplotlib.pyplot as plt
@@ -23,7 +27,6 @@ from matplotlib import animation
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../')) #This falls into Utilities path
 import Lammps.core_functions as cf
-
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -49,7 +52,7 @@ def generate_rotated_cows():
         # rotate image
         rot_img = pygame.transform.rotate(img, degree)
         img_rect = rot_img.get_rect(center = img_rect.center)
-        imagen=pygame.image.save(rot_img, "rotated_%s.png"%degree)
+        pygame.image.save(rot_img, "rotated_%s.png"%degree)
         # copy image to screen
         screen.fill((0, 0, 0))
         screen.blit(rot_img, img_rect)
@@ -73,37 +76,105 @@ def imscatter(x, y, image, ax=None, zoom=1):
         # Likely already an array...
         pass
     im = OffsetImage(image, zoom=zoom)
-    degree=0
     artists = []
     for x0, y0 in zip(x, y):
-        print x0,y0
+        angle=np.random.randint(0,359)
+        name="rotated_%s.png"%angle
+        image=plt.imread(name)
+        im = OffsetImage(image, zoom=zoom)
         ab = AnnotationBbox(im, (x0, y0), xycoords='data', frameon=False)
         artists.append(ax.add_artist(ab))
 #    ax.update_datalim(np.column_stack([x, y]))
     return artists
 
+
+
+def cow_scatter(cows,ax=None,zoom=1):
+    artists = []
+    for cow in cows:
+        image=plt.imread("cows/rotated_%s.png"%cow.angle)
+        im = OffsetImage(image, zoom=zoom)
+        ab = AnnotationBbox(im, (cow.x, cow.y), xycoords='data', frameon=False)
+        artists.append(ax.add_artist(ab))
+    return artists
+    
+    
+
+
+class cow(object):
+    def __init__(self,identity,x,y):
+        """
+        Args:
+            identity in the lammps file
+            angle rotation angle
+            omega angular velocity
+            position
+        """
+        self.identity=identity
+        self.angle=np.random.randint(0,359)
+        self.omega=np.random.randint(-5,5)
+        self.x=x
+        self.y=y
+        
+    def update_position(self,new_x,new_y):
+        self.x=new_x
+        self.y=new_y
+    
+    def update_angle(self):
+        angle=self.angle+self.omega
+        if angle<0:
+            angle=359-angle
+        angle=angle%359
+        self.angle=angle
+        
+        
+
 l_box=2.6726124191242437e+01
 
-files = glob.glob("*.cxyz")
+files = glob.glob("Trajectories/*.cxyz")
 files.sort(key=lambda f: int(filter(str.isdigit, f)))
 
-file_name="20.cxyz"
-data=cf.read_data_file(file_name).values
-
-X=data[:,2]
-Y=data[:,3]
+#intialise cow array
 
 
 
-x = X
-y = Y
-image_path = name
-fig, ax = plt.subplots()
-ax.set_xlim(0,l_box)
-ax.set_ylim(0,l_box)
-imscatter(x, y, image_path, zoom=0.3, ax=ax)
-plt.savefig('destination_path.png', format='png', dpi=1000)
-plt.show()
+
+for j,file_name in enumerate(files):
+    name=file_name.split('/')[1]
+    time=name.split('.')[0]
+    data=cf.read_data_file(file_name).values
+
+
+    #Initialise cow array
+    if j==0:
+        cows=[]
+        n,m=np.shape(data)
+        for i in xrange(n):
+            cows.append(cow(data[i,0],data[i,2],data[i,3]))
+    #Update cows
+    else:
+        for i in xrange(n):
+            cows[i].update_position(data[i,2],data[i,3])
+            cows[i].update_angle()
+            
+    #Setting up the figure
+    fig, ax = plt.subplots()
+    ax.set_xlim(0,l_box)
+    ax.set_ylim(0,l_box)
+    #ax.axis("off")
+    
+    ax.get_xaxis().set_ticks([])
+    ax.get_yaxis().set_ticks([])
+
+    for axis in ['top','bottom','left','right']:
+      ax.spines[axis].set_linewidth(3)
+
+
+    #imscatter(x, y, image_path, zoom=0.3, ax=ax)
+    fig_name="frames/frame_%s.png"%time
+    cow_scatter(cows,zoom=0.3,ax=ax)
+    plt.savefig(fig_name, format='png', dpi=300)
+
 
 
 
