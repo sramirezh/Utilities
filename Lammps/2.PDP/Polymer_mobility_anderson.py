333b5badf2e309c5eb2230dae2f2e22729e63ee5
Simon_nimbus <sr802@cam.ac.uk>
2019-05-02 18:11:24 +0100

    Addede the plateu finder

--- Lammps/2.PDP/Polymer_mobility_anderson.py
+++ Lammps/2.PDP/Polymer_mobility_anderson.py
@@ -3,7 +3,9 @@
 """
 Created on Thu Apr 11 15:14:47 2019
 Estimates the velocity only using the concentration profile.
-    
+
+
+The file prof_u.dat contains # Chunk Coord1 Ncount density/number
 Define Viscosity
 @author: sr802
 """
@@ -20,14 +22,78 @@ warnings.filterwarnings("ignore")
 import Lammps.core_functions as cf
 import pandas as pd
 
-def velocity_polymer(a,T,mu,box_size,grad_mu,cut_off):
-    data_limit=(box_size/2)*cut_off #Where the sampling volumes are outside the box
+
+
+def r_g(N):
+    """
+    Estimation for the gyration radius dependence on the number of monomers
+    from Dunweg et al"Corrections to scaling in the hydrodynamic properties of dilute polymer solutions"
+    Args:
+        N the number of monomers
+    """
+    return (0.2706*N**(1.1754)-0.32*N**(0.62))**0.5
+
+
+def r_h(N):
+    """
+    Estimation for the Hydrodynamic radius dependence on the number of monomers
+    from Dunweg et al"Corrections to scaling in the hydrodynamic properties of dilute polymer solutions"
+    Args:
+        N the number of monomers
+    """
+    return 1/(3.131*N**(-0.5877)-3.04/N)
+
+
+def group_consecutive(data):
+    """
+    Groups consecutive indexes in an array an return a list with all the groups
+    Args:
+        data 1D data array
+    Returns:
+        results a list with all the consecutive indexes grouped
+    """
+    from itertools import groupby
+    from operator import itemgetter
+    results=[]
+    for k, g in groupby(enumerate(data), lambda (i, x): i-x):
+        results.append(map(itemgetter(1), g))
+        
+         
+    return results
+         
+
+
+
+def plateau_finder(data,tol=0.0003):
+    """
+    Function that finds the plateaus of a distribution y, that has x spacing constant
+    Args:
+        data 1D data that has delta in the other dimension constant
+        tol tolerance for the variance
+    """
+    from scipy.ndimage.filters import generic_filter
+    tol=0.0003
+    filt_data = generic_filter(data, np.std, size=3)
+    plat_index=np.where(filt_data<(np.min(filt_data)+tol))[0]
+    
+    plateaus=group_consecutive(plat_index)
+    
+    return plateaus
+
+def velocity_polymer(a,T,mu,box_size,grad_mu):
+    data_limit=box_size/2 #Where the sampling volumes are outside the box
     beta=1/T
     
     data_file="prof_u.dat"
     data=pd.read_csv(data_file,sep=" ",header=None,skiprows=4).dropna(axis=1,how='all').values
     
-    indexes=np.where(data[:,1]<data_limit)[0] 
+    #Indexes inside the box, to avoid spherical shells outside the box
+    indexes_box=np.where(data[:,1]<data_limit)[0] 
+    
+    plat_indexes=max(plateau_finder(data[indexes_box,3]))
+
+    indexes=np.arange(0,plat_indexes[-1])
+    
     cs_bulk=data[indexes[-1],3]
     alpha=0.1  #Should be dependent on the 
     
@@ -56,7 +122,8 @@ def velocity_polymer(a,T,mu,box_size,grad_mu,cut_off):
     U_1=-U_0*(H+K)/a
     U=U_0+U_1
     
-#    plots(indexes,data,data_limit,y,c_excess)
+    plots(indexes,data,data_limit,y,c_excess)
+    plot_plateau(data,plat_indexes,indexes_box)
     return U_0,K,H,U_1,U
 
 
@@ -64,15 +131,16 @@ def plots(indexes,data,data_limit,y,c_excess):
     """Plots"""
     plt.close('all')
     cf.set_plot_appearance()
+    cs_bulk=data[indexes[-1],3]
     
     """Plot Solute concentration"""
     fig,ax=plt.subplots()
-    print data_limit
     ax.set_xlim(0,data_limit)
     ax.plot(data[indexes,1],data[indexes,3])
     ax.set_xlabel(r'$r[\sigma] $')
     ax.set_ylabel(r'$c_s^B [\sigma^{-3}]$')
     ymin,ymax=plt.ylim()
+    ax.axhline(y=cs_bulk, xmin=0, xmax=1,ls='--',c='black')
     fig.tight_layout()
     ax.axvline(x=a, ymin=0, ymax=1,ls='--',c='black')
     plt.savefig("Solute_concentration.pdf")
@@ -102,7 +170,7 @@ def rh_analysis(rmin,rmax):
     results=[]
     for a in rh_vect:
         
-        results.append(velocity_polymer(a,T,mu, box_size,grad_mu,cut_off))
+        results.append(velocity_polymer(a,T,mu, box_size,grad_mu))
     
     results=np.array(results)
     
@@ -139,20 +207,57 @@ def rh_analysis(rmin,rmax):
     plt.show()
 
 
-print "Remember to define the viscosity, box_size,Rh..."
+
+def plot_plateau(data,plat_indexes,indexes_box):
+    cs_bulk=data[plat_indexes[-1],3]
+    fig,ax=plt.subplots()
+    ax.plot(data[indexes_box,1],data[indexes_box,3])
+    ax.plot(data[plat_indexes,1],data[plat_indexes,3],'--')
+    ax.set_xlabel(r'$r[\sigma] $')
+    ax.set_ylabel(r'$c_s^B [\sigma^{-3}]$')
+    ymin,ymax=plt.ylim()
+    ax.axhline(y=cs_bulk, xmin=0, xmax=1,ls='--',c='black')
+    fig.tight_layout()
+    #ax.set_xlim(6,box_size/2)
+    #ax.set_ylim(0.37,0.38)
+    ax.axvline(x=a, ymin=0, ymax=1,ls='--',c='black')
+    plt.savefig("plateau.pdf")
+    plt.show()
+    
+    
+# =============================================================================
+# MAIN
+# =============================================================================
+
+print "Remember to define the viscosity"
 
 cf.set_plot_appearance()
 
 """Polymer"""
 grad_mu=0.1
-box_size=19.4754
-a_k=3.652945 #Hydrodynamic radius kirkwood 
-a_md=4.370959114216366 #Hydrodynamic radius md estimation
-T=1
-mu=1.55639001606637 
-cut_off=0.9
-velocity_polymer(a_k,T,mu, box_size,grad_mu,cut_off)
 
+
+#Automatically load the parameters
+parameters=cf.read_data_file('../data.dat').values
+N=int(cf.extract_digits(os.getcwd().split('/')[-1])[0]) #Number of monomers obtained from the path
+index_1=np.where(parameters==N)[0][0] #gets the line with the parameters of this Number of monomers
+box_size=parameters[index_1,1]
+a_k=parameters[index_1,2]
+a_md=parameters[index_1,3]
+
+
+a=a_md
+T=1
+beta=1/T
+mu=1.55639001606637 
+
+
+results=velocity_polymer(a,T,mu, box_size,grad_mu)
+print results
+
+"""
+(UNCOMMENT) Rh dependence analyisis
+"""
 #rh_analysis(2.5,7)
 
 
@@ -160,22 +265,3 @@ velocity_polymer(a_k,T,mu, box_size,grad_mu,cut_off)
 
 
 
-
-def r_g(N):
-    """
-    Estimation for the gyration radius dependence on the number of monomers
-    from Dunweg et al"Corrections to scaling in the hydrodynamic properties of dilute polymer solutions"
-    Args:
-        N the number of monomers
-    """
-    return (0.2706*N**(1.1754)-0.32*N**(0.62))**0.5
-
-
-def r_h(N):
-    """
-    Estimation for the Hydrodynamic radius dependence on the number of monomers
-    from Dunweg et al"Corrections to scaling in the hydrodynamic properties of dilute polymer solutions"
-    Args:
-        N the number of monomers
-    """
-    return 1/(3.131*N**(-0.5877)-3.04/N)
