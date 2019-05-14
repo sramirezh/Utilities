d7000322f7e1a74d276d019cd8206fdf755e07f9
MAC_simon <sramirez.hinestrosa@gmail.com>
2019-05-14 22:44:10 +0100

    added the colormap as subfigure

--- Lammps/dev/Contour/plot_streamlines.py
+++ Lammps/dev/Contour/plot_streamlines.py
@@ -36,86 +36,90 @@ For bin/cylinder, the numbering varies most rapidly in the dimension along the c
 
 import numpy as np
 import matplotlib.pyplot as plt
-data=np.loadtxt("prof2d_vel.dat",skiprows=4)
-data_rho=np.loadtxt("prof2d_con.dat",skiprows=4)
 import sys
 import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This falls into Utilities path
 import Lammps.core_functions as cf
 
-
-
-R_h=4.48
 def semi_circle(origin,r):
     tetha=np.linspace(0,np.pi,1000)
     x=r*np.cos(tetha)+origin[0]
     y=r*np.sin(tetha)+origin[1]
     
     return x,y
-    
-    
-finish=760
 
-r=data[:finish,1]
-x=data[:finish,2]
-density=data_rho[:finish,4]
-vx=data[:finish,5]
-vr=data[:finish,6]
+data=np.loadtxt("vel_static.dat",skiprows=4)
+data_rho=np.loadtxt("con_static.dat",skiprows=4)
 
+R_h=4.48
+
+def data_contour(x,y,z):
+    """
+    Retourn data ready for a contourf plot
+    """
+    n_x=len(np.unique(x))
+    n_y=len(np.unique(y))
+    
+    x=np.reshape(x,(n_y,n_x))
+    y=np.reshape(y,(n_y,n_x))
+    z=np.reshape(z,(n_y,n_x))
+    
+    return x,y,z
+    
+
+
+#Reading data and removing anomalous
+r=data[:,1]
+x=data[:,2]
 
 n_x=len(np.unique(x))
 n_r=len(np.unique(r))
 
-X,Y=np.meshgrid(x,r)
-Z=np.reshape(density,(40,19))
 
-#xreshaped=np.transpose(np.reshape(x,(20,40)))
-#rreshaped=np.transpose(np.reshape(r,(20,40)))
-#vxreshaped=np.transpose(np.reshape(vx,(20,40)))
-#vrreshaped=np.transpose(np.reshape(vr,(20,40)))
-
-xmesh,ymesh=np.mgrid[np.min(x):np.max(x):np.complex(0,n_x), np.min(r):np.max(r):np.complex(0,n_r)]
-xmesh=np.reshape(x,(19,40))
-ymesh=np.reshape(r,(19,40))
-vxmesh=np.reshape(density,(19,40))
-#
-#u = -1 - xmesh**2 + ymesh
-#v = 1 + xmesh - ymesh**2
+#Deleting the last 
+r=r[:-n_x]
+x=x[:-n_x]
+density=data_rho[:-n_x,4]
+vx=data[:-n_x,5]
+vr=data[:-n_x,6]
 
 
-#Reshaping the arrays
 
-#x=np.reshape(x,(n_x,n_y))
-#y=np.reshape(y,(n_x,n_y))
-#u=np.reshape(u,(n_x,n_y))
-#v=np.reshape(v,(n_x,n_y))
+xmesh,rmesh,density=data_contour(x,r,density)
+
 
 circle=semi_circle([10,0],R_h)
 
 cf.set_plot_appearance()
 
-#Plotting
-fig,ax=plt.subplots()
+# =============================================================================
+# Plotting
+# =============================================================================
+
+"""
+Velocity field and density contour
+"""
+delta_r=0.2
+fig,(cax,ax)=plt.subplots(nrows=2,gridspec_kw={"height_ratios":[0.05, 1]})
 plt.close('all')
-#ax.streamplot(ymesh,xmesh, v, u)
-
-#ax.streamplot (xreshaped,rreshaped,vxreshaped,vrreshaped)
-
 ax.axes.set_aspect('equal')
-cntr1=ax.contourf(xmesh,ymesh,vxmesh,alpha=0.5,cmap="RdBu_r")
-
-fig.colorbar(cntr1, ax=ax)
+cntr1=ax.contourf(xmesh,rmesh,density,alpha=0.8,cmap="RdBu_r") #cnap also could be jet
+fig.colorbar(cntr1, cax=cax, orientation='horizontal')
+cax.set_xlabel(r'c_{s}')
+cax.xaxis.set_label_position('top') 
 ax.quiver(x,r,vx,vr)
-ax.plot(circle[0],circle[1])
-ax.set_ylim(0,9)
-#ax.contourf(xreshaped,rreshaped,vxreshaped)
+ax.plot(circle[0],circle[1],color='black')
+ax.set_ylabel(r'$r=\sqrt{y^2+z^2}$')
+ax.set_xlabel(r'$x$')
+ax.set_ylim(np.min(r)-delta_r,np.max(r)+delta_r)
+fig.tight_layout()
 fig.savefig('vfield.pdf')
 
 
 
 """
-#average velocity
+#average velocity 
 """
 data2=cf.read_data_file('vel_sol.dat').get_values()
 #Substracting the velocity of the polymer
@@ -127,10 +131,9 @@ fig,ax=plt.subplots()
 every=100
 ax.plot(data2[::every,0],data2[::every,1],label='Solution')
 ax.plot(data2[::every,0],data2[::every,2],label='Inside')
-ax.plot(data2[::every,0],data2[::every,3],label='Polymer')
-ax.set_ylabel(r'$v_x $')
+ax.set_ylabel(r'$v_x-v_x^{cm}$')
 ax.set_xlabel(r'$step $')
 plt.legend()
-plt.tight_layout()
+fig.tight_layout()
 
 fig.show()
