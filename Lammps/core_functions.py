8a5aad5c0781067744bda700eb9f2cce646edd10
Simon_nimbus <sr802@cam.ac.uk>
2019-06-04 12:19:15 +0100

    Added str to bool function

--- Lammps/core_functions.py
+++ Lammps/core_functions.py
@@ -171,6 +171,17 @@ def enablePrint():
     sys.stdout = sys.__stdout__
 
 
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 
 def set_plot_appearance():
 
