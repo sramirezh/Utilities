7715c8dafb6b4484f19f90acb7d10102b89b7242
Simon_nimbus <sr802@cam.ac.uk>
2019-08-16 12:38:06 +0100

    added plot and save/load methods to the class

--- Lammps/3.Pore/GK/GK_transport.py
+++ Lammps/3.Pore/GK/GK_transport.py
@@ -19,6 +19,7 @@ import argparse
 from tqdm import tqdm  
 from joblib import Parallel, delayed
 import multiprocessing
+import cPickle as pickle
 
 try:
     from uncertainties import unumpy,ufloat
@@ -27,7 +28,16 @@ except ImportError as err2:
 import Others.Statistics.FastAverager as stat
 
 
-def compute_correlation(var1,var2,delta):
+# Put this inside the argparser
+try:
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
+except ImportError as err:
+    print err
+
+
+def compute_correlation_dt(var1,var2,delta):
     """
     This is a VERY GENERAL function
     
@@ -71,18 +81,144 @@ class flux(object):
         self.name = name
         self.times = times
         
+        #To reshape in order to slice easier in the correlation
+        if self.dimension==1: 
+            self.components = np.reshape(self.components,(len(self.components),1))
+        
         
 class correlation(object):
     def __init__(self,flux1,flux2, max_delta):
         self.flux1 = flux1
         self.flux2 = flux2
+        self.max_delta = max_delta
+        self.initial_check()
+        self.cor = (self.dimension+1)*[0]  #list to store the correlations, the last is the total
+        self.norm = (self.dimension+1)*[0]  #list to store the normalisation correlation with t=0 (var1(0) var2(0))
         
-    def initial_check():
+    def initial_check(self):
         """
         Checks if the time series are equal and the fluxes have the same number of components
         """
+        self.dimension=self.flux1.dimension
+        if self.flux1.dimension != self.flux2.dimension:
+            print "The fluxes do not have the same dimension"
+        else:
+            self.dimension=self.flux1.dimension
+            
+        if self.flux1.times.all != self.flux2.times.all:
+            print "The fluxes were not measured for the same times"
+        else:
+            self.times = self.flux1.times[:max_delta]
+            
+    def compute_correlation_dt(self,var1,var2,delta):
+        """
+        This is a VERY GENERAL function
         
+        Computes the correlation for two variables for a given delta t
+        Args:
+            var1: time series for the first variable 
+            var2: time series for the first variable
+            Note that var1 and var2 have to have the same the same time series
+            delta: every this number of steps, we take the variable 2 to compute the correlation
+        """
+        cf.blockPrint()
     
+        
+        if delta!=0:
+            var1=var1[::delta]
+            var2=var2[::delta]
+            correlation=(var1*np.roll(var2,-1,axis=0))
+            correlation=correlation[:-1]#The last contribution is the last-the initial msd
+        else:
+            correlation=var1*var2
+        
+        average=stat.fast_averager(correlation)[0]
+        cf.enablePrint()
+        
+        return ufloat(average[1],average[2]) 
+    
+    def correlate_one_d(self,dim):
+        """
+        Performs a correlation between 1d components by evaluating the products at at different delta t.
+        Args:
+            dim is the component to be evaluated
+        """
+        num_cores = multiprocessing.cpu_count()
+        var1=self.flux1.components[:,dim]
+        var2=self.flux2.components[:,dim]
+        max_delta = self.max_delta
+        norm = self.compute_correlation_dt(var1,var2,0)
+        self.norm[dim] = norm
+        cor=Parallel(n_jobs=num_cores)(delayed(compute_correlation_dt)(var1,var2,i) for i in tqdm(xrange(max_delta)))
+        self.cor[dim]=np.array(cor)/norm.nominal_value
+        
+    def evaluate(self):
+        """
+        Performs the correlations of the 1d components,calluing correlate_one_d, and adds them up to the total.
+        
+        """
+        total = np.zeros(max_delta)
+        for dim in xrange(self.dimension):
+            self.correlate_one_d(dim)
+            total = total + self.cor[dim]
+        total = total/3
+        self.cor[-1] = total
+        self.norm[-1] = 1 #It must be normalised already
+        
+    def plot_individual(self,fig,ax,dim,alpha=0.4,every=1):
+        """
+        Args:
+            ax axes object
+            fig Figure 
+            dim is the dimension, for example:in a 3D vector, 0-x, 1-y, 2-z and 3-total.
+            alpha is the transparency of the filling
+            every to not have so many points
+            The axis label is given here but it could be renamed later
+        """
+        dic_label={0:'x',1:'y',2:'z',self.dimension:'Total'}
+        y=np.array([i.n for i in self.cor[dim]])
+        y_error=np.array([i.s for i in self.cor[dim]])
+        ax.plot(self.times[::every],y[::every],label=dic_label[dim])
+        ax.fill_between(self.times, y-y_error, y+y_error ,alpha=0.4)
+        
+        
+        return fig,ax
+    
+    def plot_all(self,fig,ax,alpha=0.4):
+        for dim in xrange(self.dimension+1):
+            fig,ax = self.plot_individual(fig,ax,dim)
+        
+        ax.axhline(y=0, xmin=0, xmax=1,ls=':',c='black')
+        return fig,ax
+    
+    def save(self,file_name):
+        """
+        Saves the instance 
+        """
+        afile = open(r'%s.pkl'%file_name, 'wb')
+        pickle.dump(self, afile)
+        afile.close()    
+        
+def load_instance(file_name):
+    """
+    Loads the data structure to be used later
+    """
+    file1 = open(file_name, 'rb')
+    instance = pickle.load(file1)
+    file1.close()
+    
+    return instance
+
+
+
+    
+
+##writing the structure
+#afile = open(r'p.pkl', 'wb')
+#pickle.dump(final_p, afile)
+#afile.close()    
+        
+
 
 """
 Computes the diffusion coefficient
@@ -96,26 +232,83 @@ data1=Data.values
 times=(data1[:,0]-data1[0,0])*delta_t
 
 total_flux=flux(data1[:,[1,3,5]],times,"r'Q'")
-Solute_excess=flux(data1[:,[2,4,6]],times,"r'J_s-c_s^BQ'")
+solute_excess=flux(data1[:,[2,4,6]],times,"r'J_s-c_s^BQ'")
+
+max_delta=int(len(times)*0.004) #Maximum delta of time to measure the correlation
+
+#c11=correlation(total_flux,total_flux,max_delta)
+
+#c11.evaluate()
+#c11.correlate_one_d(0)
+c11 = load_instance("c11.pkl")
+c22 = load_instance("c22.pkl")
+c12 = load_instance("c12.pkl")
+c21 = load_instance("c21.pkl")
+
+
+
+
+plt.close('all')
+cf.set_plot_appearance()
+
+
+
+
+# =============================================================================
+# Ploting all the correlations
+# =============================================================================
+
+# For c11
+fig,ax=plt.subplots()
+
+c11.plot_all(fig,ax)
+ax.set_xscale('log')
+plt.legend(loc='upper right')
+plt.savefig("correlation11.pdf")
+
+# For c12
+fig,ax=plt.subplots()
+
+c12.plot_all(fig,ax)
+ax.set_xscale('log')
+plt.legend(loc='upper right')
+plt.savefig("correlation12.pdf")
+
+# For c21
+fig,ax=plt.subplots()
+
+c21.plot_all(fig,ax)
+ax.set_xscale('log')
+plt.legend(loc='upper right')
+plt.savefig("correlation21.pdf")
+
+# For c22
+fig,ax=plt.subplots()
+
+c22.plot_all(fig,ax)
+ax.set_xscale('log')
+plt.legend(loc='upper right')
+plt.savefig("correlation22.pdf")
 
 
 
 
 
+#
 #var1=data1[:,1]
 #var2=data1[:,2]
 #
 #t=[]
-#max_delta=int(len(times)*0.004) #Maximum delta of time to measure the correlation
+#
 #
 #
 #num_cores = multiprocessing.cpu_count()
 #
 ##correlation=compute_correlation(var1,var2,0)
 #
-#correlation=Parallel(n_jobs=num_cores)(delayed(compute_correlation)(var1,var1,i) for i in tqdm(xrange(max_delta)))
+#correlation=Parallel(n_jobs=num_cores)(delayed(compute_correlation_dt)(var1,var1,i) for i in tqdm(xrange(max_delta)))
 #
-#correlation_initial=compute_correlation(var1,var1,0) #To normalise
+#correlation_initial=compute_correlation_dt(var1,var1,0) #To normalise
 #
 #correlation_norm_x=np.array(correlation)/correlation_initial.nominal_value
 #
@@ -162,29 +355,53 @@ Solute_excess=flux(data1[:,[2,4,6]],times,"r'J_s-c_s^BQ'")
 #Total=(correlation_norm_z+correlation_norm_y+correlation_norm_x)/3
 #
 #
-#for i in xrange(max_delta):
-#    dt=times[i]
-#    t.append(dt)
+
 #
-## Put this inside the argparser
-#try:
-#    import matplotlib
-#    matplotlib.use('agg')
-#    import matplotlib.pyplot as plt
-#except ImportError as err:
-#    print err
 #cf.set_plot_appearance()
 #plt.close('all')
 #fig,ax=plt.subplots()
-#
+
 ##For x
-#y=np.array([i.n for i in correlation_norm_x])
-#y_error=np.array([i.s for i in correlation_norm_x])
+#y1=np.array([i.n for i in c11.cor[0]])
+#y1_error=np.array([i.s for i in c11.cor[0]])
+##
+#ax.plot(c11.times,y1)
+#ax.fill_between(c11.times, y1-y1_error, y1+y1_error ,alpha=0.4)
 #
-#ax.plot(t,y)
+#
+##for the old x
+#
+#y2=np.array([i.n for i in correlation_norm_x])
+#y2_error=np.array([i.s for i in correlation_norm_x])
+#
+#ax.plot(t,y2)
+#ax.fill_between(t, y2-y2_error, y2+y2_error ,alpha=0.4)
+
+##For x
+#y=np.array([i.n for i in c11.cor[1]])
+#y_error=np.array([i.s for i in c11.cor[1]])
+##
+#ax.plot(c11.times,y)
 #ax.fill_between(t, y-y_error, y+y_error ,alpha=0.4)
 #
-##For y
+#
+##For x
+#y=np.array([i.n for i in c11.cor[2]])
+#y_error=np.array([i.s for i in c11.cor[2]])
+##
+#ax.plot(c11.times,y)
+#ax.fill_between(t, y-y_error, y+y_error ,alpha=0.4)
+#
+###For total
+#y=np.array([i.n for i in c11.total_cor])
+#y_error=np.array([i.s for i in c11.total_cor])
+#
+#ax.plot(t,y,color='w')
+#ax.fill_between(t, y-y_error, y+y_error ,alpha=0.4,color='w')
+
+
+#
+##For y 
 #y=np.array([i.n for i in correlation_norm_y])
 #y_error=np.array([i.s for i in correlation_norm_y])
 #
@@ -206,31 +423,9 @@ Solute_excess=flux(data1[:,[2,4,6]],times,"r'J_s-c_s^BQ'")
 #ax.plot(t,y,color='w')
 #ax.fill_between(t, y-y_error, y+y_error ,alpha=0.4,color='w')
 #
-#ax.set_xscale('log')
-#
-#plt.savefig("correlation.pdf")
 
 
 
-#
-#
-#
-##Writing arrays of averages and errors
-#t=np.array(t)
-#msd_error=unumpy.std_devs(msd)
-#msd_average=unumpy.nominal_values(msd)
-#
-#
-#D_inst_error=unumpy.std_devs(D_inst)
-#D_inst_ave=unumpy.nominal_values(D_inst)
-#pfinal,cov=fit_line(t,msd_average,msd_error,initial_index,final_ratio,step)
-#D=pfinal[0]/(2*3)
-#D_err=np.sqrt(cov[0][0])*D
-#
-#plot_diffusion(t,msd_average,msd_error,D_inst_ave,D_inst_error,pfinal,D)
-#
-#print "The diffusion coefficient is %s +/- %s"%(D,D_err)
-#f=open("Diffusion.out",'w')
-#f.write("The diffusion coefficient is %s +/- %s \n"%(D,D_err))
-#f.close
+
+
 
