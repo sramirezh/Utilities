e36503bcb7cb91768856b859b219f499033f8e1e
Simon_nimbus <sr802@cam.ac.uk>
2019-10-18 19:14:02 +0100

    Now running but very organized structure

--- Hoomd/0.Tutorial.py
+++ Hoomd/0.Tutorial.py
@@ -30,33 +30,61 @@ ff = 0.6
 box_side = (N*vol_part/ff)**(1/3) 
 n = int(np.round(N**(1/3))) #Number of particles per dimension
 
+
+# getting the FCC lattice, following the jupyther nb
+rho = vol_part/ff
+
+vol_cell = 4*rho
+a = vol_cell**(1/3)
+
+
+
+
+# =============================================================================
+# Simple computations
+# =============================================================================
+
 hoomd.context.initialize("--mode=cpu --nthreads=12")
 
 
-system = hoomd.init.create_lattice(unitcell=hoomd.lattice.fcc(a=2), n=3)
+#Creating the HCP
+#http://lampx.tugraz.at/~hadley/ss1/crystalstructure/structures/hcp/hcp.php
 
-system.box = hoomd.data.boxdim(L = box_side)
+
+#a = 1
+#b = a
+#c = 1.633*a
+
+uc = hoomd.lattice.fcc(a = a )
+
+#uc = hoomd.lattice.unitcell(N = 4, 
+#                            a1 = [a/2,-3**(0.5)/2,0],
+#                            a2 = [a/2,3**(0.5)/2,0],
+#                            a3 = [0,0,c],
+#                            position = [[0.0,0.0,0.0],[0.5,0.5,0.0],[0.5,5.0/6.0,0.5],[0.0,1.0/3.0,0.5]],
+#                            dimensions = 3);
+                            
+                            
+system = hoomd.init.create_lattice(unitcell=uc, n = 3)
+
+#system.box = hoomd.data.boxdim(L = box_side)
 
 mc = hoomd.hpmc.integrate.sphere(d=0.2, seed=1)
 
-mc.shape_param.set('A', diameter=0.5)
+mc.shape_param.set('A', diameter=1)
 print ("The system is made of %s" %mc.get_type_shapes())
 
 
 d = hoomd.dump.gsd("trajectory.gsd", period=10, group=hoomd.group.all(), overwrite=True)
 
 
-hoomd.run(1000)
-
-mc.shape_param.set('A', diameter=1.0)
-
-hoomd.run(1000)
-
+hoomd.run(10000)
 
 snap = system.take_snapshot(all=True)
 
 positions = snap.particles.position
 
 pos_m = squareform(pdist(positions))
-
+np.fill_diagonal(pos_m,100)
+print ("\nThe minimum distance between spheres is %s" %np.min(pos_m))
 
