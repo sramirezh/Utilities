dda5fe50649a66446f3da29c95fac2c54090fad1
MAC_simon <sramirez.hinestrosa@gmail.com>
2019-11-25 10:39:24 +0000

    Created the flux gatherer for mu mu

--- Lammps/Pore/qsub/flux_gather_mu_mu.py
+++ Lammps/Pore/qsub/flux_gather_mu_mu.py
@@ -55,23 +55,23 @@ errfunc1 = lambda p, x, y, err: (y - fitfunc1(p, x)) / err #To include the error
 
 
 
-def mu_simulations(root_pattern, directory_pattern, box_volume,rho_bulk,cs_bulk,T_qs, T_ss):
+def mu_simulations(root_pattern, directory_pattern, box_volume,rho_bulk,cs_bulk,species):
 
     dictionary={'vx_Solv':r'$v^x_{f}$','vx_Solu':r'$v^x_{s}$','vx_Sol':r'$v^x_{sol}$'}
 
     # TODO This function could be included inside the class simulation_bundle
     #If the object was not saved
-    if not glob.glob("mu.pkl"):
+    if not glob.glob("mu%s.pkl"%species):
         print("\nAnalysing the mu grad simulations\n")
         bundles_mu = sr.initialise_sim_bundles(root_pattern,'mu',directory_pattern,dictionary)
         final_mu = sr.simulation_bundle(bundles_mu,'mu_bundle',3,cwd,dictionary = dictionary, ave = False)
-        final_mu.save("mu")
+        final_mu.save("mu%s"%species)
 
     # =============================================================================
     # Calculations of the transport coefficients from DP problem
     # =============================================================================
 
-    final_mu=cf.load_instance("mu.pkl") 
+    final_mu=cf.load_instance("mu%s.pkl"%species) 
     
     final_mu.average = False
     count = 0
@@ -84,9 +84,13 @@ def mu_simulations(root_pattern, directory_pattern, box_volume,rho_bulk,cs_bulk,
             n_solvents=sim.get_property('cSolv')[1][0][0]
             c_total = (n_solutes+n_solvents)/box_volume
             c_solutes = n_solutes/box_volume
+            c_solvents = n_solvents/box_volume
             
             vx_solu=ufloat(sim.get_property('vx_Solu')[1][0][0],sim.get_property('vx_Solu')[1][0][1])
-            J_s=c_solutes*vx_solu
+            vx_solv=ufloat(sim.get_property('vx_Solv')[1][0][0],sim.get_property('vx_Solv')[1][0][1])
+            
+            J_s = c_solutes*vx_solu
+            J_f = c_solvents*vx_solv
             Q=ufloat(sim.get_property('vx_Sol',exact=True)[1][0][0],sim.get_property('vx_Sol',exact=True)[1][0][1])
             pref = c_total*cs_bulk/rho_bulk
             exc_sol_flux = J_s - pref * Q
@@ -96,6 +100,7 @@ def mu_simulations(root_pattern, directory_pattern, box_volume,rho_bulk,cs_bulk,
             
             sim.add_property('Q',Q)
             sim.add_property('Js',J_s)
+            sim.add_property('Jf',J_f)
             sim.add_property('Js_exc',exc_sol_flux)
 
             count+=1
@@ -112,62 +117,15 @@ def mu_simulations(root_pattern, directory_pattern, box_volume,rho_bulk,cs_bulk,
     return final_mu
 
 
-def p_simulations(root_pattern, directory_pattern, box_volume,rho_bulk,cs_bulk,T_sq,T_qq):
-
-
-
-    dictionary={'vx_Solv':r'$v^x_{f}$','vx_Solu':r'$v^x_{s}$','vx_Sol':r'$v^x_{sol}$'}
-
-    if not glob.glob("p.pkl"):
-
-        bundles_p = sr.initialise_sim_bundles(root_pattern,'p',directory_pattern,dictionary)
-        final_p = sr.simulation_bundle(bundles_p,'p_bundle',3,cwd,dictionary = dictionary, ave = False)
-        final_p.save("p")
-
-
-    # =============================================================================
-    # Calculations for the pressure driven flow
-    # =============================================================================
-
-    final_p=cf.load_instance("p.pkl") 
-
-    for bund in final_p.simulations:
-        
-
-        for sim in bund.simulations:
-
-            n_solutes=sim.get_property('cSolu')[1][0][0]
-            n_solvents=sim.get_property('cSolv')[1][0][0]
-            c_total = (n_solutes+n_solvents)/box_volume
-            c_solutes = n_solutes/box_volume
-            
-            vx_solu=ufloat(sim.get_property('vx_Solu')[1][0][0],sim.get_property('vx_Solu')[1][0][1])
-            J_s=c_solutes*vx_solu
-            Q=ufloat(sim.get_property('vx_Sol',exact=True)[1][0][0],sim.get_property('vx_Sol',exact=True)[1][0][1])
-            pref = c_total*cs_bulk/rho_bulk
-            exc_sol_flux = J_s - pref * Q
-            
-            sim.add_property('Q',Q)
-            sim.add_property('Js',J_s)
-            sim.add_property('Js_exc',exc_sol_flux)
-                
-        #Adding specific properties to the secondary bundle
-        bund.add_property("grad_p", bund.get_property('p',exact=True)[1][0])
-        
-        bund.add_upd_properties() # To update the bundle
-        
-            
-    final_p.add_upd_properties()
-    
-    
-    return final_p
-
 
 def plot_properties(instance, x_name, y_name, x_label = None, y_label = None, plot_name=None):
     """
     TODO this could be part of the class simulation_bundle, actually I could make the get_property to return either the average or the list
     This is a partly based on simulation_bundle.plot_property that 
     """
+    
+    cf.set_plot_appearance()
+    
     y=[i.n for i in instance.get_property(y_name, exact = True)[1][0]]
     y_error=[i.s for i in instance.get_property(y_name, exact = True)[1][0]]
 
@@ -211,8 +169,9 @@ def plot_properties(instance, x_name, y_name, x_label = None, y_label = None, pl
 
 
 
-def main(m_pat, p_pat, m_dir, p_dir):
-
+def main(ms_pat, mf_pat, ms_dir, mf_dir):
+    
+    global final_mus,final_muf
 
     # =============================================================================
     # Assumptions and external parameters
@@ -237,19 +196,15 @@ def main(m_pat, p_pat, m_dir, p_dir):
     # T_qs = 0.0594056
     # T_ss = 0.0167278
 
-    # E_1.5
-    T_qq = 0.8860249969715818
-    T_sq = 0.05275808785481095
-    T_qs = 0.05275808785481095
-    T_ss = 0.0154820979540676
 
-    final_p = p_simulations(p_pat, p_dir, box_volume,rho_bulk,cs_bulk,T_sq,T_qq)
-    final_mu = mu_simulations(m_pat, m_dir, box_volume,rho_bulk,cs_bulk,T_qs,T_ss)
+    final_mus = mu_simulations(ms_pat, ms_dir, box_volume,rho_bulk,cs_bulk,"s")
+    final_muf = mu_simulations(mf_pat, mf_dir, box_volume,rho_bulk,cs_bulk,"f")
     
-    plot_properties(final_p, 'grad_p','Q', x_label = r'$-\nabla P$', y_label = r'$Q$', plot_name ="qq" )
-    plot_properties(final_p, 'grad_p','Js_exc', x_label = r'$-\nabla P$',  y_label = r'$J_s-c_s^BQ$', plot_name ="sq" )
-    plot_properties(final_mu, 'grad_mu','Q',x_label = r'$-\nabla \mu^\prime_s$', y_label = r'$Q$' ,plot_name ="qs" )
-    plot_properties(final_mu, 'grad_mu','Js_exc', x_label = r'$-\nabla \mu^\prime_s$', y_label = r'$J_s-c_s^BQ$', plot_name ="ss"  )
+    
+    plot_properties(final_mus, 'grad_mu','Js', x_label = r'$-\nabla \mu_s$', y_label = r'$J_s$', plot_name ="ss" )
+    plot_properties(final_mus, 'grad_mu','Jf', x_label = r'$-\nabla \mu_s$',  y_label = r'$J_f$', plot_name ="fs" )
+    plot_properties(final_muf, 'grad_mu','Js',x_label = r'$-\nabla \mu_f$', y_label = r'$J_s$' ,plot_name ="sf" )
+    plot_properties(final_muf, 'grad_mu','Jf', x_label = r'$-\nabla \mu_f$', y_label = r'$J_f$', plot_name ="ff"  )
     
 
 
@@ -260,14 +215,14 @@ if __name__ == "__main__":
     The arguments of this depend on the application
     """
     parser = argparse.ArgumentParser(description='Launch simulations from restart',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-m_pat', metavar='m_pat',help='Generic name of the directories with mu gradient',default='mu_force_*')
-    parser.add_argument('-p_pat', metavar='p_pat',help='Generic name of the directories with p gradient',default='p_force_*')
-    parser.add_argument('-m_dir', metavar='m_dir',help='Patter of the files inside, in this case restart are like 202000',default='[0-9]*')
-    parser.add_argument('-p_dir', metavar='p_dir',help='Patter of the files inside, in this case restart are like 202000',default='[0-9]*')
+    parser.add_argument('-ms_pat', metavar='ms_pat',help='Generic name of the directories with mu gradient',default='mus_force_*')
+    parser.add_argument('-mf_pat', metavar='mf_pat',help='Generic name of the directories with p gradient',default='muf_force_*')
+    parser.add_argument('-ms_dir', metavar='ms_dir',help='Patter of the files inside, in this case restart are like 202000',default='[0-9]*')
+    parser.add_argument('-mf_dir', metavar='mf_dir',help='Patter of the files inside, in this case restart are like 202000',default='[0-9]*')
     args = parser.parse_args()
 
     
-    main(args.m_pat,args.p_pat,args.m_dir,args.p_dir)
+    main(args.ms_pat,args.mf_pat,args.ms_dir,args.mf_dir)
     
     
     
