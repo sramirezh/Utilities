11c5046ba6aaa848b1e6e610f667f56f4bc7058e
Simon_nimbus <sr802@cam.ac.uk>
2019-12-03 13:57:10 +0000

    Added time estimations

--- Others/Statistics/FastAverager.py
+++ Others/Statistics/FastAverager.py
@@ -17,6 +17,7 @@ import os
 import sys
 from .Functions import autocorrelation_error, blocking_error
 from scipy import stats
+import time as t
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../')) #This falls into Utilities path
 import Lammps.core_functions as cf
@@ -160,18 +161,21 @@ def calculations(data, min_limit,output_file, names=None, function=False):
     """
 
     #Autocorrelation Analysis
-
+    
+    ti = t.time()
     n,m=np.shape(data_to_analyse)
-    Correlation,time=autocorrelation_error(data_to_analyse, averages)
-    error_c=np.sqrt(Correlation[0,:]*2*time/(n+1))
-
+    Correlation,time = autocorrelation_error(data_to_analyse)
+    error_c = np.sqrt(Correlation[0,:]*2*time/(n+1))
+    print ("The autocorrelation analysis took %f"%(t.time()-ti))
 
     #Simple error
     error_s=stats.sem(data_to_analyse)
     variance_s=np.var(data_to_analyse,axis=0)
 
     #Blocking analysis
+    ti = t.time()
     error_b=blocking_error(data_to_analyse)
+    print ("The blocking analysis took %f"%(t.time()-ti))
 
     if function==False:
         print("The Results are:\n")
