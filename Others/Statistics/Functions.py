27d90706ef2e819b62f68e986774dea6600bc620
Simon_nimbus <sr802@cam.ac.uk>
2019-12-03 13:57:31 +0000

    Made everything pythonic and checked with Rome

--- Others/Statistics/Functions.py
+++ Others/Statistics/Functions.py
@@ -4,36 +4,21 @@ import numpy as np
 #Blocking
 def blocking(A):
     """
-    A is the initial matrix with an even row number, do it not only for the closest integer.
+    Creates a reduced vector with the averages of consecutive pairs [see notes in ROME]
+    Args:
+        A is the initial vector
+    Returns 
+        B is the pair averaged vector
     """
-    n,m=np.shape(A)
-    B=np.zeros((int(0.5*n),m))
-    for i in range(int(0.5*n)):
-        B[i]=0.5*(A[2*i-1,:]+A[2*i,:])           
+    n = int(0.5*len(A))
+    B = 0.5*(A[0::2][:n]+A[1::2][:n])      
     return B
-
-
-
-def statistics(M):
-    """
-    Returns the average, the Error squared and the variance.
-    """
-    n,m=np.shape(M)
-    Av=np.sum(M,0)/n
-    Error=np.zeros((1,m))
-    var=np.zeros((1,m))
-    for i in range(m):
-        diff=M[:,i]-Av[i] #replace with an index
-        var[0,i]=(np.sum(diff**2))/n 
-        Error[0,i]=np.sqrt(var[0,i]/n) #Pag 9 Error of independent sampling
-    return np.append( Av,[Error,var])
     
-def autocorrelation_error (A,av):
+def autocorrelation_error (A):
     """
     Computes an autocorrelation analysis
     Args:
         A matrix with the data in columns of a matrix A[n,m]
-        av, the average of the data, av[m]
     Returns:
         C Correlation of the data 
         corrTime Correlation time of the data.
@@ -42,24 +27,25 @@ def autocorrelation_error (A,av):
     C=np.zeros((n,m)) #Autocorrelation
     corrTime=np.zeros(m)
     for k in range(m):
+        vector = A[:,k]
         i=0;
         tol=0.0001
         val=10
         sign=True
-        while val>tol and sign==True:    
-#            for j in range(n-i):
-#                C[i,k]=C[i,k]+(A[j,k]-av[k])*(A[j+i,k]-av[k])
-#            C[i,k]=C[i,k]/(n-i)
-            C[i,k] = np.cov(A[:,k],np.roll(A[:,k],i,axis=0))[0][0]
-            val=C[i,k]
+        while val>tol and sign == True:
+            if i == 0:
+                C[i,k] = np.cov(vector,vector)[0][1]
+            else:
+                C[i,k] = np.cov(vector[:-i],np.roll(vector,-i)[:-i])[0][1]
+            val = C[i,k]
             if i>0:
-                sign=C[i,k]<C[i-1,k]
-                corrTime[k]=corrTime[k]+C[i,k]/C[0,k]
-            i+=1
-    corrTime=0.5+corrTime
+                sign = C[i,k]<C[i-1,k]
+                corrTime[k] = corrTime[k]+C[i,k]/C[0,k]
+            i+= 1
+    corrTime  = 0.5 + corrTime
     return C, corrTime
 
-def blocking_error(data):
+def blocking_error(data, error = False):
     """
     Returns a zero error and average for a column that has zero in all entries
     """
@@ -68,25 +54,30 @@ def blocking_error(data):
     Results=np.zeros((MaxIter,3*m))
     Error=np.zeros((MaxIter,m)) 
     final_error=np.zeros(m)
+    variance = np.var(data,axis=0)
     for j in range(m):
-        i=0
-        diff=1
-        data1=data
+        i = 0
+        diff = 1
+        data1 = data[:,j]
         while diff>0 and i<MaxIter:    ###Always ascending results that's why it is required diff>0
-            Results[i,:]=statistics(data1)
-            data1=blocking(data1)
-            Error[i,j]=np.sqrt((2.0**i/n)*Results[i,2*m+j])
+            variance = np.var(data1, ddof = 1 )
+            data1 = blocking(data1)
+            Error[i,j] = np.sqrt((2.0**i/n)*variance)
             if i>0:
-                diff=Error[i,j]-Error[i-1,j]
-            i+=1
+                diff = Error[i,j]-Error[i-1,j]
+            i += 1
             
     for i in range(m):
         if np.size(np.nonzero(Error[:,i]))==0:  #This arises when the 
             final_error[i]=0
         else:
             final_error[i]=Error[np.max(np.nonzero(Error[:,i]))-1,i]
-        
-    return final_error
+    
+    if error == True:
+        return final_error,Error
+
+    else:
+        return final_error
 
     
     
