b32f599268da6c5ccdac9e71f9507519df0cb004
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-02-14 18:50:04 +0000

    Computing the lennard jones force

--- Lammps/Colloid/Force_on_Colloid.py
+++ Lammps/Colloid/Force_on_Colloid.py
@@ -25,19 +25,36 @@ import Lammps.core_functions as cf
 from ovito.modifiers import *
 import ovito.io as ov
 
+#
+#import sys
+#import time
+#from PyQt5.QtCore import *
+#from PyQt5.QtGui import *
+#from PyQt5.QtWidgets import *
+#
+#app = QApplication(sys.argv)
+#label = QLabel("message")
+#label.setWindowFlags(Qt.SplashScreen)
+#label.show()
+#QTimer.singleShot(6000, app.quit)
+#app.exec_()
 
-import sys
-import time
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
 
-app = QApplication(sys.argv)
-label = QLabel("message")
-label.setWindowFlags(Qt.SplashScreen)
-label.show()
-QTimer.singleShot(6000, app.quit)
-app.exec_()
+def fdivr(r, epsilon, sigma):
+    """
+    computes the lennard-jones force divided by r (f/r)
+    Args:
+        r       : the position at which the potential is computed..
+        epsilon : Energy of the minimum.
+        sigma   : Radiuos at which the potential goes to zero.
+    Returns:
+        F Lennard Jones force
+    """
+    r2 = sigma/r**2
+    r6 = r2**3
+    virial = 48*epsilon*r6*(r6 - 0.5)/r2
+
+    return virial
 
 
 def spherical_coordinates(X):
@@ -60,7 +77,7 @@ def spherical_coordinates(X):
 
 r_colloid  = 3.23
 
-node = ov.import_file("equil.dat", multiple_frames = False)
+node = ov.import_file("all.atom", multiple_frames = True)
 
 
 #Getting the properties of the box
@@ -73,47 +90,136 @@ list(data.particle_properties.keys())
 box = node.compute(0).cell # getting the properties of the box
 L = np.diag(box.matrix) 
 center = L/2.0
-
-
-pos = data.particle_properties.position.array
-types = data.particle_properties.particle_type.array
-
-
 r_cut = 2.5
-
-relative_pos = pos-center
-
-
-r = np.linalg.norm(relative_pos,axis = 1 )
-
-
-#Limiting by radius
-ind_r = np.where(r < r_colloid+r_cut)[0]
-
-
-relative_pos = relative_pos[ind_r]
-
-r_sphe = spherical_coordinates(relative_pos)
-
-
-indexes_positive = np.where(np.abs(r_sphe[:,1]) < 1)[0]
-
-
-plt.scatter(relative_pos[indexes_positive,0],relative_pos[indexes_positive,2])
+r_max = r_colloid+r_cut
 
 
 
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(relative_pos[indexes_positive,0], relative_pos[indexes_positive,1], relative_pos[indexes_positive,2])
-
-# draw sphere
-u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-x = r_colloid*np.cos(u)*np.sin(v)
-y = r_colloid*np.sin(u)*np.sin(v)
-z = r_colloid*np.cos(v)
-ax.plot_wireframe(x, y, z, color="r")
 
 
+discard = 0
+
+n_frames = node.source.num_frames
+f_x_time = []
+solu_count_time = []
+
+
+for frame in range(n_frames):
+    print ("Analysing frame %s of %s)"%(frame,n_frames))
+    
+    if frame >= discard:
+        data = node.compute(frame)
+        pos = data.particle_properties.position.array
+        types = data.particle_properties.particle_type.array
+        relative_pos = pos-center
+        
+        
+        r = np.linalg.norm(relative_pos,axis = 1 )
+        
+        
+        #Limiting by radius
+        ind_r = np.where(r < r_max)[0]
+        relative_pos = relative_pos[ind_r]
+        types = types[ind_r]
+        pos_sphe = spherical_coordinates(relative_pos)
+        
+        
+        
+        ind_solv = np.where(types == 1)
+        ind_solu = np.where(types == 2)
+        
+        
+        
+        n_theta = 15 # Number of partitions in theta defined as measured from xy plane
+        theta = np.linspace(0,np.pi,180/n_theta)
+        theta_low = theta[:-1]
+        theta_high = theta[1:]
+        
+        
+        indexes = []
+
+        
+        
+        epsilon1 = 1
+        epsilon2 = 1.5
+        sigma2 = r_colloid
+        sigma1 = r_colloid
+        
+        #Angular distribution of partilces and forces
+        
+        force = [] #
+        theta = []
+        solute = [] 
+        
+        for k,tl in enumerate(theta_low):
+            index = (np.where((np.abs(pos_sphe[:,1]) > tl) & (np.abs(pos_sphe[:,1]) <= theta_high[k]))[0])
+            indexes.append(index)
+            f_x = 0 
+            theta.append((tl+theta_high[k])/2)
+            solu_count = 0
+            for i in index:
+                if types[i] == 1:
+                    r = pos_sphe[i,0]
+                    dx = relative_pos[i,0]
+                    f_x += - fdivr(r,epsilon1,sigma1)*dx  #The minus is because is the force that the colloid feels, not the particle
+                if types[i] == 2:
+                    r = pos_sphe[i,0]
+                    dx = relative_pos[i,0]
+                    f_x += - fdivr(r,epsilon1,sigma1)*dx 
+                    solu_count += 1
+            solute.append(solu_count)
+            force.append(f_x)
+        f_x_time.append(force)
+        print (np.sum(force))
+        solu_count_time.append(solute)
+        
+            
+            
+theta = np.array(theta)*180/np.pi
+f_dist = np.average(f_x_time,axis = 0)
+solute_dist = np.average(solu_count_time,axis = 0)
+ftotal = np.sum(f_dist)
+
+total_force_time = np.sum(f_x_time, axis =1)
+
+
+#print("The average total force is %s",ftotal)
+
+
+#
+## Figures
+#fig,ax = plt.subplots()
+#
+#
+#
+#ax.plot(theta,f_x_time[0], label='Force', ls = '--')
+##ax.plot(x, rho_total_c, label='Total')  
+#ax.axhline(y=0, xmin = 0, xmax=1, ls='--',c='black')
+#ax.axvline(x=180, ymin = 0, ymax=1, ls='--',c='black')
+#
+#ax.set_xlabel(r"$x$")
+#ax.set_ylabel(r"$fx$")
+#ax.legend()
+#plt.tight_layout()
+#plt.savefig("force_angular.pdf")
+
+
+
+# =============================================================================
+# Plotting just to check
+# =============================================================================
+#from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+#plt.close('all')
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+#for indexes_positive in index:
+#    ax.scatter(relative_pos[indexes_positive,0], relative_pos[indexes_positive,1], relative_pos[indexes_positive,2])
+#
+## draw sphere
+#u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+#x = r_colloid*np.cos(u)*np.sin(v)
+#y = r_colloid*np.sin(u)*np.sin(v)
+#z = r_colloid*np.cos(v)
+#ax.plot_wireframe(x, y, z, color="r")
+#
+#plt.show()
