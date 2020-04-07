819cb2b7c7174d54c028b79aee60617fe1177ea5
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-04-07 20:25:37 +0200

    Created the universe of centroids

--- Lammps/Transport_coefficients/Analysis/molecular_analysis.py
+++ Lammps/Transport_coefficients/Analysis/molecular_analysis.py
@@ -6,24 +6,48 @@ Created on Tue Apr  7 14:04:24 2020
 @author: simon
 """
 
+import numpy as np
 import MDAnalysis as mda
 
 
 import MDAnalysis.analysis.rdf as rdf
 import matplotlib.pyplot as plt
 
-u = mda.Universe("system.data", "dcd_nvt.dcd", format="LAMMPS") 
+u = mda.Universe("system.data", "dcd_nvt.dcd", format="LAMMPS")  # The universe for all the atoms
+
+
+
+
+
 
 n_molecules = u.atoms.n_residues
-molecules = u.atoms.residues
+molecules = u.atoms.residues # I can select them here and then iterate later through the trajectory
 
 
-for ts in u.trajectory[:2]:
-    for mol in molecules:
-        centroid = mol.atoms.centroid()
-        print(centroid)
 
+
+time_steps = u.trajectory.n_frames
+
+centroids_traj = np.empty((time_steps, n_molecules, 3 ))
+
+for i,ts in enumerate(u.trajectory):
     
+    for j,mol in enumerate(molecules):
+        centroids_traj[i, j,:] = mol.atoms.centroid()
+
+
+
+# =============================================================================
+# Creating an universe for the molecules
+# =============================================================================
+
+uc = mda.Universe.empty(n_molecules, trajectory=True)
+
+uc.load_new(centroids_traj)
+
+
+#I can write an XYZ and then read it again or just analyse on the flight
+
 #    # analyze frame
 #    if take_this_frame == True:
 #    with mda.Writer('frame.data') as W:
