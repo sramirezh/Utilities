3033601166e5c18776957cc69e8749291bb76c98
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-04-09 10:29:25 +0200

    added list of non-interacting

--- Lammps/Transport_coefficients/Analysis/molecular_analysis.py
+++ Lammps/Transport_coefficients/Analysis/molecular_analysis.py
@@ -5,12 +5,24 @@ Created on Tue Apr  7 14:04:24 2020
 
 @author: simon
 """
-
+import sys
+import os
 import numpy as np
-import MDAnalysis as mda
 import matplotlib.pyplot as plt
-
+import MDAnalysis as mda
+import MDAnalysis.transformations as tr
 import MDAnalysis.analysis.rdf as rdf
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This falls into Utilities path
+import Lammps.core_functions as cf
+from scipy.spatial.distance import pdist,squareform
+
+
+
+# =============================================================================
+# Input parameters
+# =============================================================================
+
+r_max = 1.1+8.0
 
 
 u = mda.Universe("system.data", "dcd_nvt.dcd", format="LAMMPS")  # The universe for all the atoms
@@ -34,17 +46,41 @@ distances = []
 #  Unwtrapping the coordinates
 # =============================================================================
 ag = u.atoms
-transform = mda.transformations.unwrap(ag)
+transform = tr.unwrap(ag)
 u.trajectory.add_transformations(transform)
 
-for i,ts in enumerate(u.trajectory):
-    distances_t = []
-    for j,mol in enumerate(molecules):
-        centroids_traj[i, j,:] = mol.atoms.centroid(compound='residues')
-        pos = mol.atoms.positions
-        distances_t.append(np.linalg.norm(pos[1]-pos[0]))
+
+list_ni = [] # has the time step and the list of non-interacting molecules
+ratio = []
+for i,ts in enumerate(u.trajectory[:100]):
     
-    distances.append(distances_t)
+
+    list_ni_t = ["Frame %s"%ts.time]
+    # Computing the centroid
+    centroid_pos = molecules.atoms.centroid(compound='residues')
+    centroids_traj[i, :,:]  = centroid_pos
+
+    pos_m = squareform(pdist(centroid_pos))
+    indexes = np.where(pos_m>r_max)
+    
+    # seting all the non interacting particles to zero
+    pos_m[indexes] = 0
+    
+    suma = np.sum(pos_m, axis = 1)
+    
+    ni_molecules = np.where(suma == 0)[0] # Non-interacting molecules
+    
+    
+    ratio.append(len(ni_molecules)/n_molecules) # Ratio of non interacting molecules
+    
+    list_ni_t.extend(ni_molecules)
+    
+    list_ni.append(list_ni_t)
+    
+    
+    
+    
+    
 
 
 array_dist = np.transpose(np.array(distances))
@@ -86,5 +122,15 @@ plt.show()
 
 # Integrating to get the average number of particles at a given radius
 
-r_max = 1.1+8.0
+
+volume = uc.trajectory[0].volume # Assuming a constant volume
+rho_ave = uc.atoms.n_atoms/volume
+
+integrand = 4*np.pi*g_r.bins**2*g_r.rdf
+
+
+
+N_interacting = cf.integrate(g_r.bins,integrand,0,r_max)*rho_ave
+
+print(N_interacting)
 
