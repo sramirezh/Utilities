2ea8688560f2a0de232364095d5cd1870cb6ebc4
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-04-30 20:03:10 +0200

    added the pkl, and eta(tau)

--- Lammps/Transport_coefficients/Analysis/viscosity.py
+++ Lammps/Transport_coefficients/Analysis/viscosity.py
@@ -31,40 +31,72 @@ import pandas as pd
 
 import Lammps.Pore.GK.flux_correlation as fc
 
-
-
-
-
 cwd = os.getcwd() #current working directory
 
-data = cf.read_data_file("pressures.dat")
+
+def run_analysis():
+    data = cf.read_data_file("pressures.dat")
+    
+    data1 = data.values
+    times = (data1[:,0]-data1[0,0])*time_step
+    
+    
+    
+    sampling_interval = times[1]-times[0] # Assuming times are homogeneous
+    
+    
+    
+    max_delta = int(max_tau/sampling_interval)  # int(len(times)*0.1) #Maximum delta of time to measure the correlation
+    
+    
+    pxy = data['v_pxy'].values
+    
+    #pxy = np.reshape(np.)
+    
+    pxy_flux = fc.flux(pxy,times,"Pxy")
+    
+    
+    etha11 = fc.correlation(pxy_flux,pxy_flux, max_delta)
+    etha11.evaluate()
+    etha11.save("etha11")
+    
+    return etha11
+
+
+
+
+
+
+
+# =============================================================================
+# Main
+# =============================================================================
+
 
 
 time_step = 10 # In femptoseconds
-data1 = data.values
-times = (data1[:,0]-data1[0,0])*time_step
-
-
-
-sampling_interval = times[1]-times[0] # Assuming times are homogeneous
-max_tau = 20000
-
-
-max_delta = int(max_tau/sampling_interval)  # int(len(times)*0.1) #Maximum delta of time to measure the correlation
-
-
-pxy = data['v_pxy'].values
-
-#pxy = np.reshape(np.)
-
-pxy_flux = fc.flux(pxy,times,"Pxy")
-
-
-etha11 = fc.correlation(pxy_flux,pxy_flux, max_delta)
-etha11.evaluate()
-
+max_tau = 1000000 # in fs
 
 print ("Using delta_t = %s fs" %time_step)
+print ("Using max tau = %s fs"%max_tau)
+
+
+
+
+if (len(glob.glob('etha11*')) == 1):
+    
+    print ("\nThere is a pkl, we need to load etha11!\n")
+    etha11 = cf.load_instance("etha11.pkl")
+    
+else:
+    print ("There is no pkl file, so we need to analyse")
+    etha11 = run_analysis()
+    
+
+
+
+
+
 
 
 # =============================================================================
@@ -89,7 +121,7 @@ fig,ax = etha11.plot_individual(fig, ax, norm = False)
 #ax.set_ylim(-0.0005,0.0005)
 ax.plot(lammps_df["TimeDelta"]*time_step, lammps_df["v_pxy*v_pxy"],label ="Lammps")
 
-
+ax.axhline(y = 0, xmin=0, xmax=1,ls='--',c='black')
 ax.set_xscale('log')
 ax.set_xlabel(r'$\tau[fs]$')
 
@@ -124,6 +156,9 @@ prefactor = scale* prefactor
 integral = etha11.transport_coeff(1, 0, etha11.times[-1]) # In atmospheres**2*fs
 
 
+
+
+
 eta = integral * prefactor # in Pa s
 
 
@@ -132,4 +167,33 @@ print ("The viscosity is %2.4e"%eta)
 integral_lammps = cf.integrate(lammps_df["TimeDelta"]*time_step,lammps_df["v_pxy*v_pxy"],0,20000)
 
 eta_lammps = integral_lammps * prefactor
+
+
+
+
+
+# Plot eta vs tau
+
+
+fig,ax = plt.subplots()
+
+tau_array = np.arange(1,etha11.times[-1],100)
+
+eta_array = []
+
+
+for t in tau_array:
+    eta_array.append(etha11.transport_coeff(prefactor, 0, t))
+    
+    
+ax.plot(tau_array,eta_array)
+
+ax.set_xlabel(r'$\tau[fs]$')
+ax.axhline(y = eta, xmin=0, xmax=1,ls='--',c='black', label = "%s" %eta)
+ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+ax.set_ylabel(r'$\eta[Pa \cdot s]$')
+plt.legend( loc = 'upper right')
+plt.tight_layout()
+plt.savefig("eta_vs_tau.pdf")
+
 
