a05426f1fc9dda88e29050e4f4f245816e031e27
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-05-09 00:14:34 +0200

    Added the frame number

--- Lammps/DO/NEMD_Analysis/chunk_evolution.py
+++ Lammps/DO/NEMD_Analysis/chunk_evolution.py
@@ -15,14 +15,11 @@ import argparse
 import os
 import sys
 from io import StringIO
-
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This falls into Utilities path
 import Lammps.General.chunk_utilities as cu
 import Lammps.core_functions as cf
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-
-
 
 # =============================================================================
 # Reading data
@@ -36,12 +33,8 @@ fig, ax = plt.subplots()
 
 def init():
     line.set_data([], [])
-    return line,
-
-
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 25), ylim=(-1, 1))
-line, = ax.plot([], [], lw=3)
+    time_text.set_text('')
+    return line,time_text
 
 
 def animate(i, results):
@@ -50,14 +43,23 @@ def animate(i, results):
 #    x = res[i].data['vx'].values
 #    y = res[i].data['vx'].values
     line.set_data(x, y)
-    return line,
+    time_text.set_text('seq= %.1f' %i)
+    return line,time_text
+
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 25), ylim=(-1, 1))
+line, = ax.plot([], [], lw=3)
+time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+
+
 
 
 
 indexes = np.arange(0,len(results),100)
 
 anim = FuncAnimation(fig, animate, init_func=init,
-                               frames = len(indexes), interval=20, blit=True, fargs =[results])
+                               frames = len(indexes), interval=200, blit=True, fargs =[results])
 
 
 anim.save('velocity_evolution.gif', writer='imagemagick')
