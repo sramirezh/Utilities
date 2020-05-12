9594229c705e0b63a68863e5ee39b55876d7a4df
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-05-12 09:43:27 +0200

    working

--- Lammps/Transport_coefficients/Analysis/diffusion_coefficient_old.py
+++ Lammps/Transport_coefficients/Analysis/diffusion_coefficient_old.py
@@ -228,10 +228,7 @@ sim.print_params()
 # =============================================================================
 if os.path.exists("centroids_traj.npy"):
     print ("Reading 'centroids_traj.npy'")
-    centroids_traj = np.load("centroids_traj.npy")    
-
-    centroids_traj = centroids_traj[:1000]
-    
+    centroids_traj = np.load("centroids_traj.npy")        
     time_steps = len(centroids_traj)
 else:
     centroids_traj, time_steps = compute_centroids()
