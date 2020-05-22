63823798331187e070ca78e7345e0f276f3f51cb
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-05-22 07:45:05 +0200

    added all flows and the zoom

--- Lammps/DO/NEMD_Analysis/velocity_profile.py
+++ Lammps/DO/NEMD_Analysis/velocity_profile.py
@@ -12,15 +12,16 @@ import argparse
 import os
 import sys
 from io import StringIO
-
+import matplotlib.pyplot as plt
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This falls into Utilities path
 import Lammps.General.chunk_utilities as cu
 import Lammps.core_functions as cf
-import matplotlib.pyplot as plt
+import density_analysis as da
 
 
-
-chunks = cf.read_data_file("properties_short.dat") 
+fluid = da.PropertyDistribution("properties_short.dat") 
+solute = da.PropertyDistribution("Sproperties_short.dat") 
+solvent = da.PropertyDistribution("Fproperties_short.dat") 
 
 # =============================================================================
 # Plotting
@@ -29,10 +30,18 @@ cf.set_plot_appearance()
 
 plt.close("all")
 fig, ax = plt.subplots()
-chunks.plot(x = 'Coord1',y = 'vx', ax = ax, kind = 'line', legend = False,)
+fluid.data_frame.plot(x = 'Coord1',y = 'vx', ax = ax, kind = 'line', legend = False)
+solvent.data_frame.plot(x = 'Coord1',y = 'vx', ax = ax, kind = 'line', legend = False)
+solute.data_frame.plot(x = 'Coord1',y = 'vx', ax = ax, kind = 'line', legend = False)
 #ax.axvline(x = lz_min_half, ls=':',c='black')
 ax.set_xlabel(r'$z[\sigma] $')
 ax.set_ylabel(r'$v_x(z)$')
 fig.tight_layout()
+ax.legend(["Fluid", "Solvent", "Solute"])
 fig.savefig('vprofile.pdf')
 
+
+# Detail at the interface
+ax.set_xlim(0,2)
+ax.set_ylim(0,0.005)
+fig.savefig('vprofile_zoom.pdf')
