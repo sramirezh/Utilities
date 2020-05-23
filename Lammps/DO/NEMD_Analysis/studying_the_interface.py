7ff3601f4aca6adcf66602fe54db4e7391813f6c
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-05-23 16:32:37 +0200

    functions to work with the zoomed regions

--- Lammps/DO/NEMD_Analysis/studying_the_interface.py
+++ Lammps/DO/NEMD_Analysis/studying_the_interface.py
@@ -21,6 +21,26 @@ import Lammps.core_functions as cf
 import Lammps.DO.EMD.density_analysis as da
 
 
+def plot_zoom(ax,fig, xlim, ylim, name):
+    ax.set_xlim(xlim[0], xlim[1])
+    ax.set_ylim(ylim[0], ylim[1])
+    fig.tight_layout()
+    fig.savefig("%s.pdf"%name)
+    
+def get_ymax(xmax, fluid, solute, solvent, prop_name):
+    """
+    Returns the closest x to xmax (from below) and the largest value of the property 
+    at that point.
+    
+    Assumes that the property grows
+    """
+    
+    ind_x = fluid.data_frame['Coord1'].iloc[(fluid.data_frame['Coord1']- xmax ).abs().argsort()[:2]].idxmin()
+    xmax = fluid.data_frame['Coord1'].iloc[ind_x]
+    ymax = max(fluid.data_frame[prop_name].iloc[ind_x], solute.data_frame[prop_name].iloc[ind_x], solvent.data_frame[prop_name].iloc[ind_x] )
+    
+    return xmax, ymax
+ 
 fluid = da.PropertyDistribution("properties_short.dat") 
 solute = da.PropertyDistribution("Sproperties_short.dat") 
 solvent = da.PropertyDistribution("Fproperties_short.dat") 
@@ -33,30 +53,29 @@ cf.set_plot_appearance()
 plt.close("all")
 fig, ax = plt.subplots()
 
-ax = fluid.plot_property_dist("vx", ax = ax)
+fluid.plot_property_dist("vx", ax = ax)
+solvent.plot_property_dist("vx", ax = ax)
+solute.plot_property_dist("vx", ax = ax)
+ax.axhline(y=0, xmin=0, xmax=1,ls='--',c='black')
 
-#fluid.data_frame.plot(x = 'Coord1',y = 'vx', ax = ax, kind = 'line', legend = False)
-#solvent.data_frame.plot(x = 'Coord1',y = 'vx', ax = ax, kind = 'line', legend = False)
-#solute.data_frame.plot(x = 'Coord1',y = 'vx', ax = ax, kind = 'line', legend = False)
-#ax.axvline(x = lz_min_half, ls=':',c='black')
+
+ax.set_xlim(0, fluid.positions[-1])
 ax.set_xlabel(r'$z[\sigma] $')
 ax.set_ylabel(r'$v_x(z)$')
+
 fig.tight_layout()
 ax.legend(["Fluid", "Solvent", "Solute"])
 fig.savefig('vprofile.pdf')
 
-
-ymin, ymax = plt.ylim()
-
-# Detail at the interface
-ax.set_xlim(0,2)
-ax.set_ylim(ymin,0.005)
-fig.tight_layout()
-fig.savefig('vprofile_zoom.pdf')
+ymin = fluid.data_frame['vx'].min()
 
 
-# Detail at the interface
-ax.set_xlim(0,5)
-ax.set_ylim(ymin,0.03)
-fig.tight_layout()
-fig.savefig('vprofile_zoom2.pdf')
+# Getting the index closest to the limit
+xmax = 2
+xmax, ymax = get_ymax(xmax, fluid, solute, solvent, "vx" )
+plot_zoom(ax, fig,  [0,xmax], [ymin, ymax], "vprofile_zoom")
+
+
+xmax = 5
+xmax, ymax = get_ymax(xmax, fluid, solute, solvent, "vx" )
+plot_zoom(ax, fig,  [0, xmax], [ymin, ymax], "vprofile_zoom2")
