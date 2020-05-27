f57bf6148d55273fd33f6b51eb056481a911b2c1
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-05-27 20:46:31 +0200

    added logger and simulation class

--- Lammps/DO/EMD/analytic_results.py
+++ Lammps/DO/EMD/analytic_results.py
@@ -17,71 +17,90 @@ import re
 import density_analysis as da
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This falls into Utilities path
 import Lammps.core_functions as cf
+import Lammps.lammps_utilities as lu
+
+
+class SimulationEMD(lu.SimulationType):
+    def __init__(self, name, T, eta, grad_mu_s):
+        self.name = name
+        self.T = T
+        self.eta = eta
+        self.grad_mu_s = grad_mu_s
+    
+        
+            
 
 # =============================================================================
 # Main
 # =============================================================================
+logger = cf.log(__file__, os.getcwd())    
 
+# Write all the new types of simulations here
+Yoshida = SimulationEMD("Yoshida0125", 1, 1.57, -0.125)
 
+# Reading all the final averaged chunks
 solute = da.DensityDistribution("Sproperties_short.dat",'rBulk')
 solvent = da.DensityDistribution("Fproperties_short.dat",'rBulk')
 fluid = da.DensityDistribution("properties_short.dat",'rBulk')
 
-# Parameters
+# Loading the data from the simualtion
+sim_log = lu.Simulation("log.lammps")
+z_wall = sim_log.limits[0, 2]
 Kb = 1
-T = 1
-eta =  1.57
-grad_mu_s = - 0.125
 
+#  define the type of simulation
+sim = Yoshida
+sim.print_params(logger)
 
-z_wall = 0
-
+# =============================================================================
+# Computing the analytic properties
+# =============================================================================
 solute.compute_all_properties(solute.lower_limit)
 solvent.compute_all_properties(solvent.lower_limit)
 
+
+
+
+# =============================================================================
+# Getting the velocities as in Anderson1984
+# =============================================================================
 # For the solutes
-grad_c_s = grad_mu_s * solute.rho_bulk
-v_0_s = - (Kb* T)* solute.l*solute.k/eta
+grad_c_s = sim.grad_mu_s * solute.rho_bulk
+v_0_s = - (Kb* sim.T)* solute.l*solute.k/sim.eta
 vx_s = v_0_s * grad_c_s
 
 
 # For the solvents
-grad_mu_f = - (solute.rho_bulk / solvent.rho_bulk)* grad_mu_s
+grad_mu_f = - (solute.rho_bulk / solvent.rho_bulk)* sim.grad_mu_s
 grad_c_f = grad_mu_f * solvent.rho_bulk
-v_0_f = - (Kb* T)* solvent.l*solvent.k/eta
+v_0_f = - (Kb* sim.T)* solvent.l*solvent.k/sim.eta
 vx_f = v_0_f * grad_c_f
-
-
 
 vx_total = vx_s + vx_f
 
+logger.info("The contribution to the velocity due to the solutes is %s"%vx_s)
+logger.info("The contribution to the velocity due to the solvents is %s"%vx_f)
+logger.info("The predicted DO velocity is  %s" % (vx_total))
 
-print (vx_s)
-print (vx_f)
-
-
-print ("total is %s" % (vx_total))
-
-
-# =============================================================================
-# Computing the velocity using both species
-# =============================================================================
-print("The property distributions from the solutes are:" )
-print(solute.property_dist)
-#solute.plot_property_dist()
-
-
-solute.get_k(0)
-solute.plot_property_dist("integrand_k", 0,8)
-
-
-
-
-
-#  Integral
-
-
-
-cf.integrate()
+## =============================================================================
+## Computing the velocity using both species
+## =============================================================================
+#print("The property distributions from the solutes are:" )
+#print(solute.property_dist)
+##solute.plot_property_dist()
+#
+#
+#solute.get_k(0)
+#solute.plot_property_dist("integrand_k", 0,8)
+#
+#
+#
+#
+#
+##  Integral
+#
+#
+#
+#cf.integrate()
 
 
