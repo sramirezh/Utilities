c251ccfd1a3bb54feed025486b4827291eadb654
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-06-01 17:43:29 +0200

    added new cycler

--- Lammps/DO/EMD/plot_concentration_dependence.py
+++ Lammps/DO/EMD/plot_concentration_dependence.py
@@ -140,7 +140,7 @@ fig3.savefig('integrand_k.pdf')
 # Preparing the data_file
 # =============================================================================
 
-columns = ['x0', 'temp', 'press', 'solute.rhobulk', 'solvent.rhobulk',
+columns = ['x0', 'temp', 'temp_error', 'press','press_error' 'solute.rhobulk', 'solvent.rhobulk',
                          'solute.gamma', 'solute.k', 'solute.l','solute.kl',
                          'solvent.gamma', 'solvent.k', 'solvent.l', 'solvent.kl',
                          'cbSolu', 'cbSoluError']
@@ -162,8 +162,6 @@ ax.legend(loc = 'upper right')
 fig.tight_layout()
 fig.savefig('cbSolu.pdf')
 
-
-
 # Initial concentration in the box vs equilibrated concentration in the bulk 
 fig, ax = plt.subplots()
 ax.scatter(df['x0'], df['solute.rhobulk'])
@@ -173,7 +171,6 @@ ax.set_xlabel(r'$c_s^B$')
 ax.legend(loc = 'upper right')
 fig.tight_layout()
 fig.savefig('solute_rho_bulk.pdf')
-
 
 # Gamma vs concentration in the bulk
 fig, ax = plt.subplots()
@@ -215,142 +212,3 @@ ax.set_ylabel(r'$KL^*$')
 ax.legend(loc = 'upper right')
 fig.tight_layout()
 fig.savefig('solute_kl.pdf')
-
-
-
-# For the solvents
-#
-#
-#fig, ax = plt.subplots()
-#df.plot(x='x0', y='solvent.rhobulk', kind = 'scatter', ax = ax)
-#fig.tight_layout()
-#fig.savefig('solvent_rho_bulk.pdf')
-#
-#fig, ax = plt.subplots()
-#df.plot(x='solute.rhobulk',y = 'solvent.gamma', kind = 'scatter', ax = ax)
-#fig.tight_layout()
-#fig.savefig('solvent_gamma.pdf')
-#
-#fig, ax = plt.subplots()
-#df.plot(x='solute.rhobulk',y = 'solvent.l', kind = 'scatter', ax = ax)
-#fig.tight_layout()
-#fig.savefig('solvent_l.pdf')
-#
-#fig, ax = plt.subplots()
-#df.plot(x='solute.rhobulk',y = 'solvent.k', kind = 'scatter', ax = ax)
-#fig.tight_layout()
-#fig.savefig('solvent_k.pdf')
-#
-#fig, ax = plt.subplots()
-#df.plot(x='solute.rhobulk',y = 'solvent.kl', kind = 'scatter', ax = ax)
-#fig.tight_layout()
-#fig.savefig('solvent_kl.pdf')
-
-## Loading all the simulations 
-#
-## Write all the new types of simulations here
-#Yoshida = SimulationEMD("Yoshida0125", 1, 1.57, -0.125)
-#
-## Define the type of simulation
-#sim = Yoshida
-#sim.print_params(logger)
-#
-#
-#
-##folder for the small bin simulations
-#dir_f1= "4.Applying_force_0.125/1"
-#dir_f2 = "4.Applying_force_0.063/1"
-#dir_f3 = "4.Applying_force_0.025/1"
-#dir_theo = "3.Measuring"
-#
-##logger.info("The data for the main data is from %s" %dir_normal_bin)
-##logger.info("The data for the insert data is from %s" %dir_small_bin)
-#
-## Loading the NEMD data
-#fluid_f1 = da.PropertyDistribution("properties_short.dat", directory = dir_f1) 
-#fluid_f2 = da.PropertyDistribution("properties_short.dat", directory = dir_f2) 
-#fluid_f3 = da.PropertyDistribution("properties_short.dat", directory = dir_f3) 
-#
-## Loading the EMD data
-#solute = da.DensityDistribution("Sproperties_short.dat", "rBulk", directory = dir_theo) 
-#solvent = da.DensityDistribution("Fproperties_short.dat", "rBulk", directory = dir_theo) 
-
-### Loading the data for the bin 0.25 \sigma
-##fluid_s = da.DensityDistribution("properties_short.dat", "rBulk", directory = dir_small_bin) 
-#
-## =============================================================================
-## Computing the analytic properties
-## =============================================================================
-#solute.compute_all_properties(solute.lower_limit)
-#solvent.compute_all_properties(solvent.lower_limit)
-#
-#
-#grad_mu_s = [-0.125, -0.063, -0.025]
-#velocity_t_dist = []
-#velocity_s_dist = []
-#
-#for grad in grad_mu_s:
-#    grad_c_s = grad * solute.rho_bulk
-#    grad_mu_f = - (solute.rho_bulk / solvent.rho_bulk)* grad
-#    grad_c_f = grad_mu_f * solvent.rho_bulk
-#    
-#    # Getting the contribution from both species
-#    v_s = solute.vx_dist(sim, grad_c_s, solute.lower_limit) # zero for solutes
-#    v_f =solvent.vx_dist(sim, grad_c_f, solvent.lower_limit) # zero for solvents
-#    
-#    v_total = v_s + v_f
-#    velocity_s_dist.append(v_s)
-#    velocity_t_dist.append(v_total)
-#    
-## =============================================================================
-## Plotting the velocities
-## =============================================================================
-#cf.set_plot_appearance()
-#
-#plt.close("all")
-#fig1, ax1 = plt.subplots()
-#
-#
-#ax1.plot(fluid_f1.positions, fluid_f1.data_frame["vx"],marker = 'o', ls = '--', label = r'$\nabla \mu_s = -0.125$')
-#ax1.plot(solute.positions,velocity_t_dist[0],c = ax1.lines[-1].get_color(), ls = '-.')
-#ax1.plot(solute.positions,velocity_s_dist[0],c = ax1.lines[-1].get_color(), ls = ':')
-#ax1.plot(fluid_f2.positions, fluid_f2.data_frame["vx"],marker = 'o', ls = '--', label = r'$\nabla \mu_s = -0.063$')
-#ax1.plot(solute.positions,velocity_t_dist[1],c = ax1.lines[-1].get_color(), ls = '-.')
-#ax1.plot(solute.positions,velocity_s_dist[1],c = ax1.lines[-1].get_color(), ls = ':')
-#ax1.plot(fluid_f3.positions, fluid_f3.data_frame["vx"],marker = 'o', ls = '--', label = r'$\nabla \mu_s = -0.025$')
-#ax1.plot(solute.positions,velocity_t_dist[2],c = ax1.lines[-1].get_color(), ls = '-.')
-#ax1.plot(solute.positions,velocity_s_dist[2],c = ax1.lines[-1].get_color(), ls = ':')
-#
-#
-##ax.axhline(y=0, xmin=0, xmax=1,ls='--',c='black')
-#
-#
-#ax1.set_xlim(0, 30)
-#ymin, ymax = ax1.get_ylim()
-#ax1.set_ylim(0, 1.25* ymax)
-#ax1.set_xlabel(r'$z[\sigma] $')
-#ax1.set_ylabel(r'$v_x(z)$')
-#ax1.legend(loc = 'upper right')
-##
-#### Adding the insert
-##
-##left, bottom, width, height = [0.55, 0.30, 0.4, 0.30]
-##ax2 = fig1.add_axes([left, bottom, width, height])
-##
-##solute_s.plot_property_dist("vx", ax = ax2)
-##solvent_s.plot_property_dist("vx", ax = ax2)
-##fluid_s.plot_property_dist("vx", ax = ax2)
-##
-##
-##xmin = 0
-##xmax = 2
-##ax2 = cf.plot_zoom(ax2, [xmin, xmax])
-##ax2.tick_params(axis='both', which='major', labelsize = 12)
-###ax2.set_ylabel(r'$V(r)$',fontsize =17, labelpad=-5)
-###ax2.set_xlabel(r'$r$' ,fontsize =17, labelpad=-5)
-##
-##
-#fig1.tight_layout()
-#fig1.savefig('v_theo_sim.pdf')
-
-
