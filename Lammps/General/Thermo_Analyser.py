c20bd14579fc80d038dfe696b8531ebf0f14b6c7
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-06-09 20:28:37 +0200

    improved the method with linecache and clearcach

--- Lammps/General/Thermo_Analyser.py
+++ Lammps/General/Thermo_Analyser.py
@@ -16,11 +16,46 @@ Initialisation and controling inputs
 
 
 def read_chunk(file_name,i_line,f_line):
+    """
+    Args:
+    i_lines is the line where the name of the columns are, that is why the loop
+    starts at i_lines+1, 
+    f_line is the number of the line after the end of the 
+    data, so the loop reads until one line before
+    """
     data=[]
-    for i in range(i_line,f_line):
-        data.append(linecache.getline(file_name, i+1).strip('\n').split())
-    data=np.array(data)
+    
+    for i in range(i_line+1,f_line):
+        data.append(linecache.getline(file_name, i).strip('\n').split())
+    linecache.clearcache()
+    data = np.array(data, dtype = float)
     return data
+
+
+#def read_chunk(file_name, i_line, f_line):
+#    """
+#    TODO: This function is 10x slower than the one with linecache but is more 
+#    reliable, what is the problem of the linecache function above?
+#    
+#    i_line: initial line to read
+#    f_line: last line to read+1
+#    
+#    """
+#    data = []
+##    print (i_line, f_line)
+#    with open(file_name) as fp:
+#        for i, line in enumerate(fp):
+#            if i >= i_line and i<f_line-1:
+#                data.append(line.strip('\n').split())
+#            else:
+#                pass
+##                print (i)
+##    print (data[0], data[-1])
+#    data = np.array(data, dtype = float)
+#    fp.close()
+#    return data
+    
+            
 
 def save_file(data,header):
     """
@@ -33,45 +68,75 @@ def save_file(data,header):
 
 def data_extract(file_name):
     """
-    Need to change this For the time being use gawk in os and awk in ubuntu.
-    This function extracts the data from the timesteps after discarding the defined amount
+    Extracts the data from log.lammps type files and if the file did not finish,
+    still builds an array with all the results. Notice that the structure from
+    a lammps log file is as follows:
+        
+        Per MPI rank memory allocation (min/avg/max) = 2.743 | 2.782 | 3.106 Mbytes
+        Elapsed Step c_tempall c_tempfluid Press Volume Lz
+       0        0    1.0089874    1.0482703    1.2968607    10213.444      35.4657
+       .
+       .
+       .
+       10000000 10000000   0.99920245    1.0381044     1.358909    10213.444      35.4657
+       Loop time of 8850.06 on 16 procs for 10000000 steps with 7713 atoms
+        
+       
+    Args:
+        file_name: Name of the log file, usually: log.lammps
+    
+    returns:
+        total: Array with the data
+        header_string: name of the variables in the data array
+
+    TODO: At the moment it assummes that the header is the same, so the
+    thermo data is the same for all the chunks, it could be generalised so
+    if the printed data changed during the run, it could be taken into account.
     
     """
     if sys.platform=='darwin':
         awk_cmd='awk' #gwak was necessary before catalina OS
     else:
         awk_cmd='awk'
-    out,err=cf.bash_command("""grep -n "Per MPI" %s| %s -F":" '{print $1}'"""%(file_name,awk_cmd) )
-    initial_line=np.array(out.split(),dtype=int)+1
-
+       
+    # Getting the beginning of a chunk in the log
+    out,err = cf.bash_command("""grep -n "Per MPI" %s| %s -F":" '{print $1}'"""%(file_name,awk_cmd) )
+    initial_line = np.array(out.split(), dtype=int) + 1
+    
+    # Getting the end of a chunk
     out2,err=cf.bash_command("""grep -n "Loop time" %s| %s -F":" '{print $1}'"""%(file_name,awk_cmd))
-    final_line=np.array(out2.split(),dtype=int)-1
+    final_line=np.array(out2.split(), dtype=int)
+    
     #Check if there was minimization
     out3,err=cf.bash_command("""grep -n "Minimization stats" %s"""%file_name)
-
     if out3:
-        initial_line=np.delete(initial_line,0)
-        final_line=np.delete(final_line,0)
+        # Deletes the first chunk containing the minimisation
+        initial_line = np.delete(initial_line,0)
+        final_line = np.delete(final_line,0)
 
-    """Checking if the simulation did not finish"""
-    if np.size(initial_line)>np.size(final_line):
+    # Checks if the simulation finished, if not, the last line of the file is 
+    # the last line of the chunk
+    if np.size(initial_line) > np.size(final_line):
         print("\nThe log file is incomplete!, lets analyse it in anycase \n")
-        out3,err=cf.bash_command("""wc -l %s |awk '{print $1}'"""%file_name)
-        line=int(out3.split()[0])
-        final_line=np.append(final_line,line)
+        out3,err = cf.bash_command("""wc -l %s |awk '{print $1}'"""%file_name)
+        line = int(out3.split()[0])
+        final_line = np.append(final_line,line)
 
 
-    header=linecache.getline(file_name, initial_line[0]).strip('\n').split()
-    header_string=" ".join(header)
-    number_chunks=np.size(initial_line)
+    header = linecache.getline(file_name, initial_line[0]).strip('\n').split()
+    linecache.clearcache()
+    header_string = " ".join(header)
+    number_chunks = np.size(initial_line)
 
     for i in range(number_chunks):
-        if i==0:
-            total=read_chunk(file_name, initial_line[i],final_line[i])
+        if i == 0:
+            total = read_chunk(file_name, initial_line[i], final_line[i])
         else:
-            data=read_chunk(file_name,initial_line[i],final_line[i])
-            total=np.delete(total,-1,axis=0) #As there are repeated timesteps, I choose to keep the last version of the variables
-            total=np.vstack([total,data])
+            data = read_chunk(file_name, initial_line[i], final_line[i])
+            #The last step of the nth chunk is equal to the first step in 
+            # the next one, so we need to delete it
+            total=np.delete(total, -1, axis=0) 
+            total=np.vstack([total, data])
 
 
     return total, header_string
@@ -94,7 +159,7 @@ def discard_data(data,nmin):
 
     return data
 
-def thermo_analyser(file_name,minimum = 0.3):
+def thermo_analyser(file_name, minimum = 0.3):
     """
     Args:
         file_name
