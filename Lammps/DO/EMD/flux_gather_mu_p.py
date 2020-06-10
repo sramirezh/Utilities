f805793ad9044d282d5c2d0cbd6871c9793cc3a8
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-06-10 12:04:30 +0200

    addapting to p mu simulations

--- Lammps/DO/EMD/flux_gather_mu_p.py
+++ Lammps/DO/EMD/flux_gather_mu_p.py
@@ -55,25 +55,55 @@ errfunc1 = lambda p, x, y, err: (y - fitfunc1(p, x)) / err #To include the error
 
 
 
-
-def mu_simulations(root_pattern, directory_pattern,rho_bulk,cs_bulk,species):
+# TODO This function could be included inside the class simulation_bundle
+def build_bundle_mu(root_pattern, directory_pattern, rho_bulk, cs_bulk, sim_type):
+    """
+    SPECIFIC FUNCTION FOR THIS PROBLEM
+    
+    
+    Constructs a bundle of simulations based on a directory tree.
+    suppose a directory tree as follows:
+         |-4.Applying_force_0.025
+         | |-1
+         | |-2
+         | |-3
+         | |-4
+         |-4.Applying_force_0.030
+         | |-1
+         | |-2
+         | |-3
+         | |-4
+         
+    And creates the specific parameters for this analysis
+         
+    Args: 
+        root_pattern: in the example above "4.Applying_force_*"
+        directory_pattern: the runs inside each root, in the example "[0-9]*"
+        rho_bulk: Density of the fluid in the bulk
+        cs_bulk: Density on solutes in the bulk
+        sim_type: identifier of the simulations, could be "p", "mu_s", etc
+    
+    Returns:
+        final_mu: an instance of sr.simulation_bundle containing all the 
+                    information from the simulation bundle
+        
+    """
 
     dictionary = {'vx_Solv':r'$v^x_{f}$','vx_Solu':r'$v^x_{s}$','vx_Sol':r'$v^x_{sol}$'}
-
-    # TODO This function could be included inside the class simulation_bundle
-    #If the object was not saved
-    if not glob.glob("mu%s.pkl"%species):
+# =============================================================================
+#     Create or read the bundle
+# =============================================================================
+    if not glob.glob("%s"%sim_type):
         print("\nAnalysing the mu grad simulations\n")
         bundles_mu = sr.initialise_sim_bundles(root_pattern,'mu',directory_pattern,dictionary)
-        final_mu = sr.simulation_bundle(bundles_mu,'mu_bundle',3,cwd,dictionary = dictionary, ave = False)
-        final_mu.save("mu%s"%species)
+        final_mu = sr.simulation_bundle(bundles_mu,sim_type,3,cwd,dictionary = dictionary, ave = False)
+        final_mu.save("%s"%sim_type)
+    else:
+        final_mu = final_mu = cf.load_instance("%s"%sim_type) 
 
     # =============================================================================
     # Calculations of the transport coefficients from DP problem
     # =============================================================================
-
-    final_mu = cf.load_instance("mu%s.pkl"%species) 
-    
     final_mu.average = False
     count = 0
     for bund in final_mu.simulations:
@@ -84,24 +114,22 @@ def mu_simulations(root_pattern, directory_pattern,rho_bulk,cs_bulk,species):
             n_solutes = sim.get_property('cSolu')[1][0][0]
             n_solvents = sim.get_property('cSolv')[1][0][0]
             c_total =  (n_solutes+n_solvents)/sim.volume 
-            c_solvents = n_solvents/sim.volume
+#            c_solvents = n_solvents/sim.volume
             c_solutes = n_solutes/sim.volume
-            
+#            
             vx_solu = ufloat(sim.get_property('vx_Solu')[1][0][0],sim.get_property('vx_Solu')[1][0][1])
-            vx_solv = ufloat(sim.get_property('vx_Solv')[1][0][0],sim.get_property('vx_Solv')[1][0][1])
+#            vx_solv = ufloat(sim.get_property('vx_Solv')[1][0][0],sim.get_property('vx_Solv')[1][0][1])
             
             J_s = c_solutes*vx_solu
-            J_f = c_solvents*vx_solv
             Q = ufloat(sim.get_property('vx_Sol',exact=True)[1][0][0],sim.get_property('vx_Sol',exact=True)[1][0][1])
-            pref = c_total*cs_bulk/rho_bulk
+            pref = c_total * cs_bulk/rho_bulk
             exc_sol_flux = J_s - pref * Q
             
             
             #Adding specific properties to the individual simulations
             
-            sim.add_property('Q',Q)
+            sim.add_property('Q', Q)
             sim.add_property('Js',J_s)
-            sim.add_property('Jf',J_f)
             sim.add_property('Js_exc',exc_sol_flux)
 
             count+=1
@@ -109,13 +137,104 @@ def mu_simulations(root_pattern, directory_pattern,rho_bulk,cs_bulk,species):
         #Adding specific properties to the secondary bundle
         
         bund.add_property('grad_mu',rho_bulk/(rho_bulk-cs_bulk)*float(bund.get_property('mu',exact=True)[1][0]))
-        bund.add_upd_properties() # To update the bundle
+#        bund.add_upd_properties() # To update the bundle
 
 
-    final_mu.add_upd_properties()
+#    final_mu.add_upd_properties()
     
     
     return final_mu
+
+
+
+def build_bundle_p(root_pattern, directory_pattern, rho_bulk, cs_bulk, sim_type):
+    """
+    SPECIFIC FUNCTION FOR THIS PROBLEM
+    
+    
+    Constructs a bundle of simulations based on a directory tree.
+    suppose a directory tree as follows:
+         |-4.Applying_force_0.025
+         | |-1
+         | |-2
+         | |-3
+         | |-4
+         |-4.Applying_force_0.030
+         | |-1
+         | |-2
+         | |-3
+         | |-4
+         
+    And creates the specific parameters for this analysis
+         
+    Args: 
+        root_pattern: in the example above "4.Applying_force_*"
+        directory_pattern: the runs inside each root, in the example "[0-9]*"
+        rho_bulk: Density of the fluid in the bulk
+        cs_bulk: Density on solutes in the bulk
+        sim_type: identifier of the simulations, could be "p", "mu_s", etc
+    
+    Returns:
+        final_mu: an instance of sr.simulation_bundle containing all the 
+                    information from the simulation bundle
+        
+    """
+
+    dictionary = {'vx_Solv':r'$v^x_{f}$','vx_Solu':r'$v^x_{s}$','vx_Sol':r'$v^x_{sol}$'}
+# =============================================================================
+#     Create or read the bundle
+# =============================================================================
+    if not glob.glob("%s"%sim_type):
+        print("\nAnalysing the mu grad simulations\n")
+        bundles_p = sr.initialise_sim_bundles(root_pattern,'p',directory_pattern,dictionary)
+        final_p = sr.simulation_bundle(bundles_p,sim_type,3,cwd,dictionary = dictionary, ave = False)
+        final_p.save("%s"%sim_type)
+    else:
+        final_p = cf.load_instance("%s"%sim_type) 
+
+    # =============================================================================
+    # Calculations of the transport coefficients from DP problem
+    # =============================================================================
+    final_p.average = False
+    count = 0
+    for bund in final_p.simulations:
+        
+        #This is just to compute the excess of solute flux
+        
+        for sim in bund.simulations:
+            n_solutes = sim.get_property('cSolu')[1][0][0]
+            n_solvents = sim.get_property('cSolv')[1][0][0]
+            c_total =  (n_solutes+n_solvents)/sim.volume 
+#            c_solvents = n_solvents/sim.volume
+            c_solutes = n_solutes/sim.volume
+#            
+            vx_solu = ufloat(sim.get_property('vx_Solu')[1][0][0],sim.get_property('vx_Solu')[1][0][1])
+#            vx_solv = ufloat(sim.get_property('vx_Solv')[1][0][0],sim.get_property('vx_Solv')[1][0][1])
+            
+            J_s = c_solutes*vx_solu
+            Q = ufloat(sim.get_property('vx_Sol',exact=True)[1][0][0],sim.get_property('vx_Sol',exact=True)[1][0][1])
+            pref = c_total * cs_bulk/rho_bulk
+            exc_sol_flux = J_s - pref * Q
+            
+            
+            #Adding specific properties to the individual simulations
+            
+            sim.add_property('Q', Q)
+            sim.add_property('Js',J_s)
+            sim.add_property('Js_exc',exc_sol_flux)
+
+            count+=1
+        
+        #Adding specific properties to the secondary bundle
+        
+#        bund.add_property('grad_mu',rho_bulk/(rho_bulk-cs_bulk)*float(bund.get_property('mu',exact=True)[1][0]))
+#        bund.add_upd_properties() # To update the bundle
+
+
+#    final_p.add_upd_properties()
+    
+    
+    return final_p
 
 
 
@@ -197,10 +316,10 @@ def main(ms_pat, mp_pat, ms_dir, mp_dir):
     # T_ss = 0.0167278
 
 
-    final_mus = mu_simulations(ms_pat, ms_dir,rho_bulk,cs_bulk,"s")
-    final_p = mu_simulations(mp_pat, mp_dir, rho_bulk, cs_bulk,"p")
+    final_mus = build_bundle_mu(ms_pat, ms_dir,rho_bulk,cs_bulk,"s")
+    final_p = build_bundle_p(mp_pat, mp_dir, rho_bulk, cs_bulk,"p")
     
-#    
+    
 #    plot_properties(final_mus, 'mu','Js', x_label = r'$-\nabla \mu_s$', y_label = r'$J_s$', plot_name ="ss" )
 #    plot_properties(final_mus, 'mu','Jf', x_label = r'$-\nabla \mu_s$',  y_label = r'$J_f$', plot_name ="fs" )
 #    plot_properties(final_p, 'mu','Js',x_label = r'$-\nabla \mu_f$', y_label = r'$J_s$' ,plot_name ="sf" )
