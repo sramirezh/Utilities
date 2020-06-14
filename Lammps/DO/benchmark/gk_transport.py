364adf19b01de05fa601013a1ca21c3350a8aa3e
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-06-14 20:04:57 +0200

    generalised to read inside folders and subruns

--- Lammps/DO/benchmark/gk_transport.py
+++ Lammps/DO/benchmark/gk_transport.py
@@ -12,6 +12,9 @@ and if new combinations of the fluxes are wanted, use:
     v_vx_Solv v_vx_Solu v_vx_Sol v_cSolu v_cSolv
     
 Run inside 6.GJ_analysis
+
+Define which folder pattern to use
+
 Based on viscosity from DO project
 @author: simon
 """
@@ -23,8 +26,8 @@ import sys
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import uncertainties as un
 import glob
+from scipy.stats import sem
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This falls into Utilities path
 import Lammps.core_functions as cf
 import Lammps.lammps_utilities as lu
@@ -35,89 +38,180 @@ import Lammps.Pore.GK.flux_correlation as fc
 
 
 
-def run_analysis(flux_file, time_step, max_tau):
-    """
-    Specific analysis where all the fluxes are their components are defined
-    
-    """
+class SimulationGK(lu.SimulationType):
+    def __init__(self, name, time_step, max_tau, tau_integration, temp,
+                 log_file, flux_file):
+        """
+        Args:
+            name: Identifier of the simulation
+            time_step: Simulation time_step
+            max_tau: maximum length of the correlation analysis
+            tau_integration: upper time limit of the correlation integration
+            temp: system temperature
+            log_file: lammps log file
+            flux file: file containing all the fluxes
+            
+            
+        Other atributes:
+            sampling_interval
+        """
+        self.name = name
+        self.time_step = time_step
+        self.max_tau = max_tau
+        self.tau_integration = tau_integration
+        self.temp = temp
+        self.log_file = log_file
+        self.flux_file = flux_file
+        self.flux_file_prefix = self.flux_file.split('.')[0]
+        
+    def get_thermo(self):
+        """
+        Gets the thermo data as an instance of lu.SImulation
+        
+        """
+        
+        self.thermo = lu.Simulation(self.log_file)
+        
 
-    data = cf.read_data_file(flux_file)
+    def run_analysis(self):
+        """
+        This asssumes that the cwd is where all the files are
+        TODO: this could be a method of the SimulationGK class
+        Specific analysis where all the fluxes are their components are defined
+        
+        """
     
-    data1 = data.values
-    times = (data1[:,0]-data1[0,0]) * time_step
+        data = cf.read_data_file(self.flux_file)
+        
+        data1 = data.values
+        times = (data1[:,0]-data1[0,0]) * self.time_step
+        
+        
+        # Assuming times are homogeneous
+        self.sampling_interval = times[1]-times[0] 
     
+        # int(len(times)*0.1) #Maximum delta of time to measure the correlation
+        max_delta = int(self.max_tau/self.sampling_interval)  
+        
+        # Name of the components
+        Q_components = ["v_Qx", "v_Qy", "v_Qz"]
+        Js_components = ["v_Jsx_exc", "v_Jsy_exc", "v_Jsz_exc" ]
+        
+        Q = data[Q_components].values
+        Js = data[Js_components].values
+        
+        Q_flux = fc.flux(Q ,times, "Q")
+        Js_flux = fc.flux(Js ,times, "J_s")
+        
+        #  Creating the correlation instance
+        m_qs = fc.correlation(Q_flux, Js_flux, max_delta)
+        m_qs.evaluate()
+        m_qs.save("Mqs_%s"%sim.save_name)
+        
+        return m_qs
     
-    # Assuming times are homogeneous
-    sampling_interval = times[1]-times[0] 
-
-    # int(len(times)*0.1) #Maximum delta of time to measure the correlation
-    max_delta = int(max_tau/sampling_interval)  
-    
-    # Name of the components
-    Q_components = ["v_Qx", "v_Qy", "v_Qz"]
-    Js_components = ["v_Jsx_exc", "v_Jsy_exc", "v_Jsz_exc" ]
-    
-    Q = data[Q_components].values
-    Js = data[Js_components].values
-    
-    Q_flux = fc.flux(Q ,times, "Q")
-    Js_flux = fc.flux(Js ,times, "J_s")
-    
-    
-    etha11 = fc.correlation(Q_flux, Js_flux, max_delta)
-    etha11.evaluate()
-    etha11.save("etha11")
-    
-    return etha11
+    @property
+    def save_name(self):
+        """
+        Returns the sufix to save the output files, if the flux file has a number
+        adds this to the original name of the simulation
+        """
+        self.number = cf.extract_digits(self.flux_file)
+        
+        # If there are no numbers, the name does not change
+        if len(self.number) == 0:
+            self.number =''
+        else:
+            self.number = int(self.number[0])
+        
+        save_name =  '%s%s'%(self.name,self.number)
+        return save_name
 
 # =============================================================================
 # Main
 # =============================================================================
-logger = cf.log(__file__, os.getcwd())
+plot_dir = "plots"
+if not os.path.exists(plot_dir):
+    os.makedirs(plot_dir)
+    
+logger = cf.log(__file__, os.getcwd(),plot_dir)   
 
-time_step = 0.005 # In LJ time
-max_tau = 1000 # in LJ time  Max tau to analyse
-tau_integration = 10 # limit for the integration
-Kb = 1  
-Temperature = 1
-log_file = "log.lammps"
-flux_file = "fluxes.dat"
 
+# =============================================================================
+# Simulation type definitions
+# =============================================================================
+
+# High sampling frequency
+high = SimulationGK("high", 0.005, 1000, 150, 1, "log.lammps", "fluxes.dat") 
+low = SimulationGK("low", 0.005, 1000, 150, 1, "log.lammps", "fluxes_low.dat") 
+
+# Define the type of simulation
+sim = high.copy
+sim.print_params(logger)
 
 # Getting the sampling times
 #spd = lu.read_value_from("log.lammps", "fluxes.dat")
 
-logger.info("Using delta_t = %s " %time_step)
-logger.info("Using max tau = %s for the analysis "%max_tau)
-logger.info("Using integration tau = %s for the autocorrelation "%tau_integration)
-logger.info("Getting the box size from %s" %log_file)
+folder_pattern = '2.1'
 
-volume, limits = lu.read_box_limits("log.lammps", -1)
+logger.info("\nUsing the folder pattern %s"%folder_pattern )
 
-prefactor = volume / (Kb * Temperature)
+folders = glob.glob(folder_pattern)
+
+logger.info("Analysing the results in %s"%folders )
+
+# Array to store all the correlations
+correlation_dist =[]
+transport_coeff=[]
+cwd = os.getcwd()
+
+for i, folder in enumerate(folders):
+    Kb = 1
+    logger.info("Analysing the folder %s"%folder)
+    # Computing this only once, assuming all runs have the same
+    os.chdir(folder)
+    partial_runs = glob.glob("%s*"%sim.flux_file_prefix)
+    for j, file in enumerate(partial_runs):
+        if i == 0 and j == 0:            
+            # Heigth of the volume where the fluxes are measured
+            sim.get_thermo()
+            lz_system = sim.thermo.limits[1,2] - sim.thermo.limits[0,2]
+            lz_box = lu.read_value_from("in.geom", 'rSystem')[-1]
+            
+            # We need the volume where the fluxes were measured
+            volume_eff = sim.thermo.volume * lz_box/lz_system
+            
+            prefactor = volume_eff / (Kb * sim.temp)
+        
+        # The name of the 
+        sim.flux_file = file
+        logger.info("Analysing the file %s"%file)
+        # =============================================================================
+        # Computing the viscosity from the stress data
+        # =============================================================================
+        
+        # Loading the correlations if they were already computed, this saves almost 50 minutes in 16 cores (dexter)
+        if (len(glob.glob("Mqs_%s.pkl"%sim.save_name)) == 1):
+            
+            logger.info("\nThere is a pkl, we need to load etha11!\n")
+            m_qs = cf.load_instance("Mqs_%s.pkl"%sim.save_name)
+            
+        else:
+            logger.info("There is no pkl file, so we need to analyse")
+            m_qs = sim.run_analysis()
+        
+        
+        transport_coeff.append( prefactor * m_qs.transport_coeff(0, sim.tau_integration) )
+        
+        # Getting all the correlation distributions only in x
+        correlation_dist.append(m_qs.cor[0])
+    os.chdir(cwd)
+
+
+
 
 # =============================================================================
-# Computing the viscosity from the stress data
-# =============================================================================
-
-# Loading the correlations if they were already computed, this saves almost 50 minutes in 16 cores (dexter)
-if (len(glob.glob('etha11*')) == 1):
-    
-    logger.info("\nThere is a pkl, we need to load etha11!\n")
-    etha11 = cf.load_instance("etha11.pkl")
-    
-else:
-    logger.info("There is no pkl file, so we need to analyse")
-    etha11 = run_analysis(flux_file, time_step, max_tau)
-    
-    
-integral = etha11.transport_coeff(1, 0, tau_integration) 
-eta_tau_int = integral * prefactor 
-
-#logger.info("The viscosity is %2.5f+/- %2.5f in LJ units"%(eta_tau_int.n, eta_tau_int.s))
-
-# =============================================================================
-# Ploting all the correlations
+# Ploting all the correlations only for the last etha
 # =============================================================================
 
 plt.close('all')
@@ -126,32 +220,63 @@ cf.set_plot_appearance()
 #For c11
 fig,ax = plt.subplots()
 
-fig,ax = etha11.plot_all(fig, ax, norm = False)
+fig,ax = m_qs.plot_all(fig, ax, norm = False)
 
 #ax.set_xlim(1000,2000)
 #ax.set_ylim(-0.0005,0.0005)
 #ax.plot(lammps_df["TimeDelta"]*time_step, lammps_df["v_pxy*v_pxy"],label =r"$P_{xy}^{Lammps}$")
 
-ax.axvline(x = tau_integration,ls='-.',c='black')
+ax.axvline(x = sim.tau_integration,ls='-.',c='black')
 ax.axhline(y = 0, xmin=0, xmax=1,ls='--',c='black')
 
 ax.set_xscale('log')
 xmin,xmax = ax.get_xlim()
-ax.set_xlim(xmin, max_tau)
+ax.set_xlim(xmin, sim.max_tau)
 ax.set_xlabel(r'$t^*$')
 
 plt.legend([r'$xx$',r'$yy$',r'$zz$',"Total"], loc = 'upper right', fontsize = 12, ncol = 2)
-ax.set_ylabel(r'$\langle %s(t^*) %s(0)\rangle$'%(etha11.flux1.name,etha11.flux2.name))
+ax.set_ylabel(r'$\langle %s(t^*) %s(0)\rangle$'%(m_qs.flux1.name, m_qs.flux2.name))
 plt.tight_layout()
-plt.savefig("correlation11.pdf")
-
+plt.savefig("%s/c_qs_%s.pdf"%(plot_dir,sim.save_name))
+#
+#
+#
+#
 ## =============================================================================
-## # Plot eta vs tau
+## Some operations on the correlation distribution in the x-direction
+## =============================================================================
+#correlation_dist = prefactor * np.array(correlation_dist)
+#corr_dist_ave = np.average(correlation_dist, axis = 0)
+#corr_dist_error = sem(correlation_dist, axis = 0)
+#
+#
+#
+## Now plotting
+#fig,ax = plt.subplots()
+#
+#ax.plot(etha11.times, corr_dist_ave)
+#ax.fill_between(etha11.times, corr_dist_ave - corr_dist_error, corr_dist_ave + corr_dist_error, alpha=0.4)
+#ax.axvline(x = tau_integration,ls='-.',c='black')
+#ax.axhline(y = 0, xmin=0, xmax=1,ls='--',c='black')
+#
+#ax.set_xscale('log')
+#xmin,xmax = ax.get_xlim()
+#ax.set_xlim(xmin, max_tau)
+#ax.set_xlabel(r'$t^*$')
+#
+#plt.legend([r'$xx$',r'$yy$',r'$zz$',"Total"], loc = 'upper right', fontsize = 12, ncol = 2)
+#ax.set_ylabel(r'$\langle %s(t^*) %s(0)\rangle$'%(etha11.flux1.name,etha11.flux2.name))
+#plt.tight_layout()
+#plt.savefig("%s/correlation_ave_xx.pdf"%plot_dir)
+#
+#
+## =============================================================================
+## # Plot eta vs tau for the average correlation distribution in x
 ## =============================================================================
 #
 #fig,ax = plt.subplots()
 #
-#tau_array = np.linspace(etha11.times[1], etha11.times[-1])
+#tau_array = np.linspace([1], etha11.times[-1])
 ## Need to add the integration time
 #tau_array = np.sort(np.append(tau_array, tau_integration)) 
 #
@@ -161,12 +286,8 @@ plt.savefig("correlation11.pdf")
 #
 #for t in tau_array:
 #    # Taking only the nominal value
-#    eta = prefactor * etha11.transport_coeff(1, 0, t)
-#    error = 0
-#    if isinstance(eta, un.UFloat):
-#        error = eta.s
-#        eta = eta.n
-#        
+#    eta = cf.integrate(etha11.times, corr_dist_ave, 0, t,)
+#    error = 0        
 #    eta_error.append(error)
 #    eta_array.append(eta)
 #
@@ -176,14 +297,14 @@ plt.savefig("correlation11.pdf")
 #ax.plot(tau_array,eta_array)
 #ax.fill_between(tau_array, eta_array - eta_error, eta_array + eta_error, alpha=0.4)
 #ax.set_xlabel(r'$t^*$')
-#ax.axhline(y = eta_tau_int.n , xmin=0, xmax=1,ls='--',c='black', label = r'$\eta^* = %2.4f$' %eta_tau_int.n)
-#ax.axvline(x = tau_integration,ls='-.',c='black')
-#xmin,xmax = ax.get_xlim()
-#ymin,ymax = ax.get_ylim()
-#ax.set_xlim(0, etha11.times[-1])
-#ax.set_ylim(0, ymax)
+##ax.axhline(y = eta_tau_int.n , xmin=0, xmax=1,ls='--',c='black', label = r'$\eta^* = %2.4f$' %eta_tau_int.n)
+##ax.axvline(x = tau_integration,ls='-.',c='black')
+##xmin,xmax = ax.get_xlim()
+##ymin,ymax = ax.get_ylim()
+###ax.set_xlim(0, etha11.times[-1])
+###ax.set_ylim(0, ymax)
 #ax.set_ylabel(r'$\eta^*$')
 #plt.legend( loc = 'lower right')
 #plt.tight_layout()
-#plt.savefig("eta_vs_tau.pdf")
-
+#plt.savefig("%s/Mqs_vs_tau.pdf"%plot_dir)
+#
