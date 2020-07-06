0803ba5a68021d4ce2626a4e41e4d4c10a0cf064
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-07-06 11:22:30 +0100

    changed names to eta

--- Lammps/Transport_coefficients/Analysis/viscosity.py
+++ Lammps/Transport_coefficients/Analysis/viscosity.py
@@ -14,28 +14,15 @@ import sys
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import uncertainties as un
+from uncertainties import ufloat
+import glob
+import time
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This falls into Utilities path
 import Lammps.core_functions as cf
-from scipy.spatial.distance import pdist,squareform
-from tqdm import tqdm
-from uncertainties import unumpy,ufloat
-import Others.Statistics.FastAverager as stat
-from scipy import optimize
-import glob
-from joblib import Parallel, delayed
-import multiprocessing
-import pandas as pd
 import Lammps.lammps_utilities as lu
-
-
-
 import Lammps.Pore.GK.flux_correlation as fc
 
 cwd = os.getcwd() #current working directory
-
-
-
 
 class SimulationViscosity(lu.SimulationType):
 
@@ -83,14 +70,14 @@ class SimulationViscosity(lu.SimulationType):
         
         #pxy = np.reshape(np.)
         
-        p_flux = fc.flux(p_off_diag ,times,"P")
+        pressures = fc.flux(p_off_diag ,times,"P")
         
+        # Creating the correlation object
+        eta = fc.correlation(pressures, pressures, self.max_delta)
+        eta.evaluate_acf()
+        eta.save("eta")
         
-        etha11 = fc.correlation(p_flux, p_flux, self.max_delta)
-        etha11.evaluate_acf()
-        etha11.save("etha11")
-        
-        return etha11
+        return eta
     
     def _get_thermo(self):
         """
@@ -135,7 +122,7 @@ class SimulationViscosity(lu.SimulationType):
 
 # High sampling frequency
 N2 = SimulationViscosity("N2", 1000000,1000000, "log.lammps", "pressures.dat") 
-Octane = SimulationViscosity("Octane", 200000, 200000, "log.lammps", "pressures.dat") 
+Octane = SimulationViscosity("Octane", 200000, 100000, "log.lammps", "pressures.dat") 
 
 cwd = os.getcwd()
 plot_dir = "plots/0.viscosity"
@@ -152,20 +139,24 @@ sim.time_step = lu.read_value_from("input.lmp", 'myStep')[0]
 sim.print_params(logger)
 
 # Loading the correlations if they were already computed, this saves almost 50 minutes in 16 cores (dexter)
-if (len(glob.glob('etha11*')) == 1):
+if (len(glob.glob('eta.pkl')) == 1):
     
-    logger.info ("\nThere is a pkl, we need to load etha11!\n")
-    etha11 = cf.load_instance("etha11.pkl")
+    logger.info ("\nThere is a pkl, we need to load eta!\n")
+    t0 = time.time()
+    eta = cf.load_instance("eta.pkl")
+    logger.info ("It took %2.4f sec to load the file"%(time.time()-t0))
     
 else:
     logger.info ("There is no pkl file, so we need to analyse")
-    etha11 = sim.run_analysis()
+    eta = sim.run_analysis()
     
+
+ 
 
 # =============================================================================
 # Ploting all the correlations
 # =============================================================================
-
+t0 = time.time()
 # Loading lammps, on the fly calculations
 lammps_df = cf.read_data_file("profile.gk.2d")
 lammps_df = lammps_df.iloc[-400:]
@@ -176,20 +167,26 @@ cf.set_plot_appearance()
 #For c11
 fig,ax = plt.subplots()
 
-fig,ax = etha11.plot_individual(fig, ax, norm = False, dim = 3, every = 10)
+ax = eta.plot_individual(ax, norm = False, dim = 3)
 
 #ax.set_xlim(1000,2000)
 #ax.set_ylim(-0.0005,0.0005)
-#ax.plot(lammps_df["TimeDelta"].values[::10] * sim.time_step, 
-#        lammps_df["v_pxy*v_pxy"].values[::10],label =r"$P_{xy}^{Lammps}$")
 ax.axhline(y = 0, xmin=0, xmax=1,ls='--',c='black')
 ax.set_xscale('log')
 ax.set_xlabel(r'$\tau[fs]$')
-
-plt.legend( loc = 'upper right', fontsize = 12, ncol = 2)
-ax.set_ylabel(r'$\langle %s(\tau) %s(0)\rangle$'%(etha11.flux1.name,etha11.flux2.name))
+ax.set_ylabel(r'$\langle %s(\tau) %s(0)\rangle$'%(eta.flux1.name,eta.flux2.name))
 fig.tight_layout()
-fig.savefig("%s/correlation11.pdf"%sim.plot_dir)
+plt.savefig("%s/correlation.pdf"%sim.plot_dir)
+
+ax.plot(lammps_df["TimeDelta"].values[::10] * sim.time_step, 
+        lammps_df["v_pxy*v_pxy"].values[::10],label =r"$P_{xy}^{Lammps}$")
+fig.legend( loc = 'upper right', fontsize = 12, ncol = 2)
+fig.tight_layout()
+plt.savefig("%s/correlation_lammps.pdf"%sim.plot_dir)
+
+
+
+logger.info("It took %2.4f sec sec to create the correlation plots"%(time.time()-t0))
 
 # =============================================================================
 # # Plot eta vs tau
@@ -197,7 +194,7 @@ fig.savefig("%s/correlation11.pdf"%sim.plot_dir)
 
 fig,ax = plt.subplots()
 
-tau_array = np.linspace(etha11.times[0], etha11.times[-1], 20)
+tau_array = np.linspace(eta.times[0], eta.times[-1], 20)
 # Need to add the integration time
 tau_array = np.sort(np.append(tau_array, sim.tau_integration)) 
 
@@ -208,46 +205,41 @@ eta_interval = []
 #TODO be sure that the integration is continuos beween the intervals
 initial_t = 0
 for t in tau_array:
-    eta_interval.append(sim.prefactor * etha11.transport_coeff_comp(initial_t, t, -1))
+    eta_interval.append(sim.prefactor * eta.transport_coeff_comp(initial_t, t, -1))
     initial_t = t
     
 
 # Neef to rewrite the first term, very quick fix
 eta_interval[0] = ufloat(0,0)
 
-eta = np.cumsum(eta_interval)
-eta_error = np.array([el.s for el in eta])
-eta_array = np.array([el.n for el in eta])
+eta_tau = np.cumsum(eta_interval)
+eta_error = np.array([el.s for el in eta_tau])
+eta_array = np.array([el.n for el in eta_tau])
+
+
+# Getting the data explicitly for tau_integration
+index = np.where(tau_array==sim.tau_integration)[0][0]
     
 ax.plot(tau_array,eta_array)
 ax.fill_between(tau_array, eta_array - eta_error, eta_array + eta_error, alpha=0.4)
 ax.set_xlabel(r'$t^*$')
-#ax.axhline(y = eta_tau_int.n , xmin=0, xmax=1,ls='--',c='black', label = r'$\eta^* = %2.4f$' %eta_tau_int.n)
-#ax.axvline(x = tau_integration,ls='-.',c='black')
+ax.axhline(y = eta_array[index] , xmin=0, xmax=1,ls='--',c='black', label = r'$\eta^* = %2.4f$' %eta_array[index])
+ax.axvline(x = sim.tau_integration,ls='-.',c='black')
 xmin,xmax = ax.get_xlim()
 ymin,ymax = ax.get_ylim()
-ax.set_xlim(0, etha11.times[-1])
+ax.set_xlim(0, eta.times[-1])
 ax.set_ylim(0, ymax)
 ax.set_ylabel(r'$\eta^*$')
 plt.tight_layout()
 plt.savefig("%s/eta_vs_tau.pdf"%sim.plot_dir)
-#
-#
-#
-#
-#
-#integral = etha11.transport_coeff(0, etha11.times[-1]) # In atmospheres**2*fs
-#
-#eta = integral * sim.prefactor # in Pa s
-#
-## It is a unumpy array
-#total_eta = sum(eta)/len(eta)
+
+
 #logger.info("The 3 components of the viscosity are %s" %eta)
-#logger.info("The average viscosity is %2.4e +/- %2.4e [Pa s]"%(total_eta.n, total_eta.s))
-#
-#integral_lammps = cf.integrate(lammps_df["TimeDelta"].values*sim.time_step,lammps_df["v_pxy*v_pxy"].values,0,20000)
-#
-#eta_lammps = integral_lammps * sim.prefactor
-#logger.info("On the fly estimation is %s" %eta_lammps)
+logger.info("The average viscosity is %2.4e +/- %2.4e [Pa s]"%(eta_tau[index].n, eta_tau[index].s))
+
+integral_lammps = cf.integrate(lammps_df["TimeDelta"].values*sim.time_step,lammps_df["v_pxy*v_pxy"].values,0,20000)
+
+eta_lammps = integral_lammps * sim.prefactor
+logger.info("On the fly estimation is %s" %eta_lammps)
 
 
