d709b99e5545dd0d22477f28cdb87e8f76978277
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-08-19 18:29:08 +0100

    added more comparison plots

--- Lammps/PDP/trajectory_analysis/Diffusion_coefficient.py
+++ Lammps/PDP/trajectory_analysis/Diffusion_coefficient.py
@@ -19,6 +19,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../')) #This fall
 import Lammps.core_functions as cf
 import Lammps.General.thermo_analyser as ta
 
+
 cwd = os.getcwd() #current working directory
 dir_path = os.path.dirname(os.path.realpath(__file__))#Path of this python script
 
@@ -80,8 +81,11 @@ def compute_one_time(pos_init,fil):
 Main
 *******************************************************************************
 """
-        
-logger = cf.log(__file__, os.getcwd())    
+plot_dir = "plots/diffusion_coefficient"
+if not os.path.exists(plot_dir):
+    os.makedirs(plot_dir)
+    
+logger = cf.log(__file__, os.getcwd(), plot_dir)    
 
 
 delta_t= 0.005
@@ -91,8 +95,9 @@ times_l, msd_l = lammps_MSD(delta_t, logger)
 input_files = glob.glob("conf/dump*")
 input_files.sort(key=lambda f: list(filter(str.isdigit, f)))
 times = cf.extract_digits(input_files)[0] * delta_t
-num_conf=len(times)
-Box,L=pa.Box_limits(input_files[0])
+times = times - times[0]
+num_conf = len(times)
+Box,L = pa.Box_limits(input_files[0])
 
 Data_init=pd.read_csv(input_files[0],sep=" ",skiprows=9,dtype=np.float64,header=None).sort_values(0).values[:,:-1]
 pos_init=pa.real_position(Data_init,L) #Real positions of all the atoms
@@ -109,11 +114,48 @@ results=Parallel(n_jobs=num_cores,verbose=10)(delayed(compute_one_time)(pos_init
 #    results.append(compute_one_time(pos_init,fil))
 
 results=np.array(results)
-#plt.plot(times_l,msd_l,label="lammps")
-#plt.plot(times,results[:,3],label="My_algorithm")
 
-out=np.polyfit(times,results[:,3],1)
+# Need to discard the short times
+discard = int(0.5*len(times))
+out = np.polyfit(times[discard:],results[discard:,3],1)
 
-D=out[0]/(2*3)
+D = out[0]/(2*3)
 
 logger.info("The diffusion coefficient from My calculations is %s"%D)
+
+
+# =============================================================================
+# Ploting the MSD
+# =============================================================================
+
+cf.set_plot_appearance()
+
+# Ploting the comparison between BD-NEMD, FD-NEMD and Theory
+fig, ax = plt.subplots()
+
+
+ax.plot(times_l, msd_l,label="lammps")
+ax.plot(times,results[:,3],label="My algorithm")
+ax.plot(times[discard:], np.polyval(out,times[discard:]), label ="my fit")
+
+
+#ax.errorbar(dcv[:,0], -dcv[:,1], yerr= dcv[:,2], label="BD-NEMD", ls = '--', fmt='o')
+#ax.errorbar(fdmd_free[:,0],-fdmd_free[:,1],yerr= fdmd_free[:,2],label="FD-NEMD", fmt='o', ls = '--')
+##ax.errorbar(fdmd_fixed[:,0],fdmd_fixed[:,1],yerr= fdmd_fixed[:,2],label="EFMD fixed", fmt='o')
+##ax.scatter(theory[:,0], theory[:,1], label="Theory v0")
+#ax.plot(theory[:,0], theory[:,2], label="Theory", marker = "s", ls = '--')
+##ax.plot(theory_r[:,0], theory_r[:,2], label="Theory $r=R_h$")
+#ax.axhline(y=0, xmin=0, xmax=1,ls='--',c='black')
+#
+#ax.set_xlabel(r'$\varepsilon_{cs} $')
+#ax.set_ylabel(r'$v_c^x$')
+#ymin,ymax=plt.ylim()
+ax.set_ylim(0, None)
+ax.set_xlim(0, None)
+ax.legend(loc='upper left')
+fig.tight_layout()
+plt.savefig("%s/msd_comparison.pdf"%plot_dir)
+logger.info("plotted %s/msd_comparison.pdf"%plot_dir)
+
+
+
