17ca023393a80c42778e9e01d555fd867b34172e
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-10-14 15:55:07 +0200

    making it depend on the functions

--- Others/Fitting/viscosity.py
+++ Others/Fitting/viscosity.py
@@ -7,22 +7,27 @@ the GLJ paper
 @author: simon
 """
 import numpy as np
-
-import multid_fit_liquid_jup as mf
 import matplotlib.pyplot as plt
 import copy
+import fitting_functions as ff
+import os 
+import sys
+Utilities_path = os.path.join(os.path.dirname(__file__), '../../')
+sys.path.append(Utilities_path) #This falls into Utilities path
+import Lammps.core_functions as cf
+
 
 coefficient_file = 'fit_coefficients_liquid_rc2.0.txt'
 values_file = 'viscosity_liquid_rc2.0.txt'
 
 
 # Reading the coefficients and exponents
-m_values, n_values, coefficients = mf.read_coeff_file(coefficient_file)
+m_values, n_values, coefficients = ff.read_coeff_file(coefficient_file)
 
 # Reading the energy
-header, data = mf.read_file(values_file)
+header, data = ff.read_file(values_file)
 
-poly_eta = mf.polynomial(n_values, m_values,[1],[1],[1,0],[1,0])
+poly_eta = ff.polynomial(n_values, m_values,[1],[1],[1,0],[1,0])
 
 x_e = data[:,0] # Density
 y_e = data[:,1] # Temperature
@@ -38,25 +43,17 @@ variables = copy.deepcopy(data[:,0:2])
 # Getting beta
 variables[:,1] = 1/variables[:,1]
 
-er_results_eta = mf.test_prediction(coefficients, np.transpose(variables), data[:,2], poly_eta)
+er_results_eta = ff.test_prediction(coefficients, np.transpose(variables), data[:,2], poly_eta)
 
 z_predicted = er_results_eta[:,1]
 
-mf.set_plot_appearance()
+cf.set_plot_appearance()
 
 #plt.close('all')
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(111, projection='3d')
 ax1.scatter( y_e, x_e, z_e, zdir='z',marker='.',label="Simulation",color='r')
 ax1.scatter(y_e, x_e, z_predicted, zdir='z',marker='.',label="Fitting",color='b')
-ax1.set_xlabel(r'$\rho$', labelpad = 5)
-ax1.set_ylabel(r'$T$', labelpad = 5)
+ax1.set_xlabel(r'$T$', labelpad = 5)
+ax1.set_ylabel(r'$\rho$', labelpad = 5)
 ax1.set_zlabel(r'$\eta$', labelpad = 5)
-
-
-
-
-# Testing the polynomial by hand
-
-
-
