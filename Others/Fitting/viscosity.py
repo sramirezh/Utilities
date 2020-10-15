d4fc446a6d168dfee05243c7b7f0ea78d59db8a1
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-10-15 18:44:36 +0200

    adapted to the new functions

--- Others/Fitting/viscosity.py
+++ Others/Fitting/viscosity.py
@@ -16,14 +16,11 @@ Utilities_path = os.path.join(os.path.dirname(__file__), '../../')
 sys.path.append(Utilities_path) #This falls into Utilities path
 import Lammps.core_functions as cf
 
-
-
 # =============================================================================
 # Input parameters
 # =============================================================================
 coefficient_file = 'fit_coefficients_liquid_rc2.0.txt'
 values_file = 'viscosity_liquid_rc2.0.txt'
-
 
 # =============================================================================
 # Reading files
@@ -35,27 +32,20 @@ m_values, n_values, coefficients = ff.read_coeff_file(coefficient_file)
 header, data = ff.read_file(values_file)
 
 # Definining the polynomial
-poly_eta = ff.polynomial(n_values, m_values,[1],[1],[1,0],[1,0])
-
+poly_eta = ff.Polynomial(n_values, m_values,[1],[1],[1,0],[1,0])
 
 # Check the header varible but in general the variables are like this
 x_e = data[:,0] # Density
 y_e = data[:,1] # Temperature
 z_e = data[:,2] # Viscosity
 
-
 variables = copy.deepcopy(data[:,0:2])
 
 # Getting beta
 variables[:,1] = 1/variables[:,1]
 
-
-
 # Evaluating the polynomial at the given ponts
-er_results_eta = ff.test_prediction(coefficients, np.transpose(variables), data[:,2], poly_eta, 0)
-z_predicted = er_results_eta[:,1]
-
-
+z_predicted = poly_eta.evaluate(np.transpose(variables), coefficients)
 
 # =============================================================================
 # Plotting the points and predicted
@@ -69,3 +59,4 @@ ax1.scatter(y_e, x_e, z_predicted, zdir='z',marker='.',label="Fitting",color='b'
 ax1.set_xlabel(r'$T$', labelpad = 5)
 ax1.set_ylabel(r'$\rho$', labelpad = 5)
 ax1.set_zlabel(r'$\eta$', labelpad = 5)
+fig1.show()
