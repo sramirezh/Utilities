149a4a88ed8d840a285f85979947ee99d0ba672c
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-10-15 18:28:55 +0200

    Adapted to the new functions

--- Others/Fitting/fit_liquid.py
+++ Others/Fitting/fit_liquid.py
@@ -6,12 +6,9 @@ Created on Thu May  9 11:10:12 2019
 @author: simon
 """
 import numpy as np
-from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from cycler import cycler
 import os
-import shutil
 import sys
 import fitting_functions as ff
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../')) #This falls into Utilities path
@@ -20,17 +17,6 @@ import Lammps.core_functions as cf
 # =============================================================================
 # MAIN
 # =============================================================================
-
-
-    
-#def main(rho_ref,beta_ref,deg_x,deg_y,p_ref,e_ref,file_p,file_e):
-#    '''
-#    x is the density
-#    y is beta
-#    z is the property
-#    '''
-    
-
 
 rho_ref = 0 #0.32
 beta_ref = 0  #0 1/1.04
@@ -41,126 +27,94 @@ file_e='energy/energy_liquid_rc2.0.txt'
 p_ref = 0
 e_ref = 0
 
-
-
-# =============================================================================
-#     Here is where I define the polynomial for the pressure
-# =============================================================================
-#    print "Basic information about the polynomial for the Pressure"
-poly_p = ff.polynomial(deg_x,deg_y,[1,-1],[1],[1,0],[1,0])
-
-
-
-# =============================================================================
-#     Here is where I define the polynomial for the pressure
-# =============================================================================
-#    print "Basic information about the polynomial for the Energy"
-poly_e  = ff.polynomial(deg_x,deg_y,[1],[1,0],[1,0],[1,-1])
-
-
-
 print("rho_ref = %s"%rho_ref)
 print("beta_ref = %s"%beta_ref)
 
 # =============================================================================
-#     #Reading the data for the pressure
+#     Here is where I define the polynomials
 # =============================================================================
+
+# Pressure
+poly_p = ff.Polynomial(deg_x,deg_y,[1,-1],[1],[1,0],[1,0])
+# Energy
+poly_e  = ff.Polynomial(deg_x,deg_y,[1],[1,0],[1,0],[1,-1])
+
+# =============================================================================
+#     Reading the data
+# =============================================================================
+
+# Pressure
 x_p, y_p, z_p, zerr_p = ff.read_data(file_p, p_ref)
 x_p = x_p - rho_ref
 y_p = y_p - beta_ref
 
-
-
-# =============================================================================
-#     #Reading the data for the energy
-# =============================================================================
+# Energy
 x_e, y_e, z_e, zerr_e = ff.read_data(file_e, e_ref)
 x_e = x_e - rho_ref
 y_e = y_e - beta_ref
 
+# =============================================================================
+#     Simultaneous fitting
+# =============================================================================
 
-
-
-#Simultaneous Fitting
 popt, pcov, variables = ff.fit_general([[x_p, y_p, z_p, zerr_p], poly_p],[[x_e,y_e,z_e,zerr_e],poly_e])
 
 
 plt.close('all')
 
-
-#
-## =============================================================================
-##     #Testing for Pressure
-## =============================================================================
-#print ('\nTesting the pressure results\n')
-#variables_p=np.stack((x_p,y_p),axis=0)
-#er_results_p=test_prediction(popt,variables_p,z_p,poly_p)
-#
-#outputs(popt,pcov,er_results_p, deg_x, deg_y,'p')
-#
-#fig1 = plt.figure()
-#ax = fig1.add_subplot(111, projection='3d')
-#ax.scatter(x_p, y_p, z_p, zdir='z',marker='.',label="Simulation",color='r')
-#
-#
-#
-#
-#
-##Creating the surface
-#x,y=np.meshgrid(np.linspace(np.min(x_p),np.max(x_p),20),np.linspace(np.min(y_p),np.max(y_p),20))
-#
-#
-#
-#z=np.asarray(x)
-#variables_p=np.stack((x.flatten(),y.flatten()),axis=0)
-#er_results=test_prediction(popt,variables_p,z.flatten(),poly_p)
-#z_mesh=np.reshape(er_results[:,1],np.shape(x))
-#ax.plot_wireframe(x,y,z_mesh,color='b')
-#
-#ax.set_xlabel(r'$\rho-\rho^*$')
-#ax.set_ylabel(r'$\beta-\beta^*$')
-#ax.set_zlabel(r'$\Delta \beta P_{exc}$')
-#fig1.legend()
-#fig1.show()
-#fig1.savefig("3Dplot_p.pdf")
-
-
 # =============================================================================
-#     #Testing for Energy
+#     Testing Results
 # =============================================================================
 print ('\nTesting the energy results\n')
-variables_e=np.stack((x_e,y_e),axis=0)
-er_results_e = ff.test_prediction(popt,variables_e,z_e,poly_e, e_ref)
-#    
-ff.outputs(popt,pcov,er_results_e, deg_x, deg_y,'e')
-#    
+
+# Computing the predictions
+variables_e = np.stack((x_e,y_e),axis=0)
+z_e_predicted = poly_e.evaluate(variables_e, popt)
+
+# Computing the error
+er_results_e = ff.test_prediction(z_e, z_e_predicted, e_ref)
+
+# Writing the output data
+ff.outputs(popt,pcov, er_results_e, deg_x, deg_y,'e')
+
+# Plotting the results
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111, projection='3d')
+ax1.scatter(x_e, y_e, z_e, zdir = 'z',marker ='.', label="Simulation", color ='r')
+ax1.scatter(x_e, y_e, z_e_predicted, zdir='z', label="Fitting", color = 'black')
+ax1.set_xlabel(r'$\rho-\rho^*$')
+ax1.set_ylabel(r'$\beta-\beta^*$')
+ax1.set_zlabel(r'$\Delta \rho \ e_{exc}$')
+
+fig1.legend()
+fig1.show()
+fig1.savefig("3Dplot_e.pdf")
+
+
+print ('\nTesting the pressure results\n')
+
+# Computing the predictions
+variables_p = np.stack((x_e,y_e),axis=0)
+z_p_predicted = poly_p.evaluate(variables_p, popt)
+
+# Computing the error
+er_results_p = ff.test_prediction(z_p, z_p_predicted, p_ref)
+
+# Writing the output data
+ff.outputs(popt, pcov, er_results_p, deg_x, deg_y, 'p')
+
+# Plotting the results
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111, projection='3d')
-ax2.scatter(x_e, y_e, z_e, zdir='z',marker='.',label="Simulation",color='r')
-ax2.scatter(x_e, y_e, er_results_e[:,1], zdir='z',label="Fitting",color='black')
-#    
-#Creating the surface
-#x,y=np.meshgrid(np.linspace(np.min(x_e),np.max(x_e),20),np.linspace(np.min(y_e),np.max(y_e),20))
-
-
-
-
-#z=np.asarray(x)
-#variables_e=np.stack((x.flatten(),y.flatten()),axis=0)
-#er_results=test_prediction(popt,variables_e,z.flatten(),poly_e)
-#z_mesh=np.reshape(er_results[:,1],np.shape(x))
-#ax2.plot_wireframe(x,y,z_mesh,color='b')
+ax2.scatter(x_p, y_p, z_p, zdir='z',marker='.',label="Simulation", color='r')
+ax2.scatter(x_p, y_p, z_p_predicted, zdir='z',label="Fitting", color='black')
 ax2.set_xlabel(r'$\rho-\rho^*$')
 ax2.set_ylabel(r'$\beta-\beta^*$')
-ax2.set_zlabel(r'$\Delta \rho \ e_{exc}$')
+ax2.set_zlabel(r'$\Delta \beta\ P_{exc}$')
 
 fig2.legend()
 fig2.show()
-fig2.savefig("3Dplot_e.pdf")
-
-
-
-
+fig2.savefig("3Dplot_p.pdf")
 
 ## =============================================================================
 ## #    Single fitting
